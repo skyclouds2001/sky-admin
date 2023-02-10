e9e26b9609578febc8bf49a40caf29e6f1839753
skyclouds2001 <skyclouds2001@163.com>
2023-02-10 16:34:11 +0800

    feat: update layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
+import { onBeforeMount } from 'vue'
 import { ElConfigProvider } from 'element-plus'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import { locale } from 'dayjs'
+import 'dayjs/locale/zh-cn'
+import LayoutContainer from '@/components/LayoutContainer.vue'
+
+onBeforeMount(() => {
+  locale('zh-cn')
+})
 </script>
 
 <template>
   <ElConfigProvider :locale="zhCn">
-    <router-view></router-view>
+    <LayoutContainer>
+      <router-view></router-view>
+    </LayoutContainer>
   </ElConfigProvider>
 </template>
 
---
+++ src/components/LayoutContainer.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ElContainer, ElAside, ElHeader, ElMain, ElFooter } from 'element-plus'
+</script>
+
+<template>
+  <ElContainer class="page-container">
+    <ElAside>Aside</ElAside>
+    <ElContainer>
+      <ElHeader>Header</ElHeader>
+      <ElMain>
+        <slot></slot>
+      </ElMain>
+      <ElFooter>Footer</ElFooter>
+    </ElContainer>
+  </ElContainer>
+</template>
+
+<style scoped lang="scss">
+.page-container {
+  width: 100vw;
+  min-height: 100vh;
+}
+</style>

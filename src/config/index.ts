5d43793bfc4ab2b4d5b39c826c9a638273e5e997
skyclouds2001 <skyclouds2001@163.com>
2023-02-13 16:16:16 +0800

    feat: update application config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,10 @@
+import { name, version } from 'package.json'
+
+/** 项目名称 */
+export const APPLICATION_NAME = name
+
+/** 项目版本 */
+export const APPLICATION_VERSION = version
+
 /** 服务器 HOST */
 export const SERVER_HOST = 'http://localhost:8080'

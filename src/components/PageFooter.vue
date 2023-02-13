5b520f1cd53ac470f03b9a079dc2da1e08635940
skyclouds2001 <skyclouds2001@163.com>
2023-02-13 11:30:26 +0800

    feat: update page footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <a href="https://github.com/skyclouds2001" target="_blank" class="page-footer">2023-present © SkyAdmin By skyclouds2001.</a>
+  <div class="page-footer">
+    <a href="https://github.com/skyclouds2001" target="_blank">2023-present © SkyAdmin By skyclouds2001.</a>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .page-footer {
   @apply flex justify-center items-center;
-  @apply text-black;
 
   height: 100%;
+
+  a {
+    @apply text-black;
+  }
 }
 </style>

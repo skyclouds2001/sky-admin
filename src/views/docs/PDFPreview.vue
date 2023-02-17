d24bbdd50300d34804a5ca5a3e3bd138560d00e6
skyclouds2001 <skyclouds2001@163.com>
2023-02-17 14:42:09 +0800

    feat: remove unused pdf preview method

--- src/views/docs/PDFPreview.vue
+++ src/views/docs/PDFPreview.vue
@@ -1,12 +1,6 @@
 <script setup lang="ts">
-// import { onBeforeMount } from 'vue'
 import { ElSpace } from 'element-plus'
 import { DEMO_PDF_SRC } from '@/config'
-// import { renderPDF } from '@/util/pdf-preview'
-
-// onBeforeMount(() => {
-//   renderPDF(DEMO_PDF_SRC, document.getElementById('pdf-preview') as HTMLCanvasElement)
-// })
 </script>
 
 <template>
@@ -15,12 +9,6 @@ import { DEMO_PDF_SRC } from '@/config'
       <div class="text-xl font-bold py-4">原生 iframe 方案</div>
       <iframe class="pdf-preview" name="pdf-preview" title="pdf-preview" referrerpolicy="no-referrer" :src="DEMO_PDF_SRC"></iframe>
     </div>
-    <!--
-    <div class="overflow-hidden">
-      <div class="text-xl font-bold py-4">pdf.js 方案</div>
-      <canvas id="pdf-preview"></canvas>
-    </div>
-    -->
   </el-space>
 </template>
 

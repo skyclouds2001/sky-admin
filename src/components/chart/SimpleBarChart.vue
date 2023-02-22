c72b8f5c6b2ddebf2e930f9bc216486649e3b2dc
skyclouds2001 <skyclouds2001@163.com>
2023-02-22 18:17:28 +0800

    feat: update simple bar chart example

--- src/components/chart/SimpleBarChart.vue
+++ src/components/chart/SimpleBarChart.vue
@@ -3,36 +3,62 @@ import { onMounted } from 'vue'
 import { echarts, type EchartsOptions } from '@/lib'
 
 onMounted(() => {
-  const el = document.getElementById('simple-bar-chart') as HTMLDivElement
-
-  const options: EchartsOptions = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        type: 'bar',
-        data: [120, 200, 150, 80, 70, 110, 130],
-        // showBackground: true,
-        // backgroundStyle: {
-        //   color: 'rgba(180, 180, 180, 0.2)',
-        // },
-      },
-    ],
-  }
-
-  echarts.getInstanceByDom(el)?.dispose()
-
   echarts
-    .init(el, 'light', {
+    .init(document.getElementById('simple-bar-chart') as HTMLDivElement, 'light', {
       width: 1000,
       height: 600,
     })
-    .setOption(options)
+    .setOption<EchartsOptions>({
+      title: {
+        show: true,
+        text: 'Simple Bar Chart',
+        left: 'center',
+        top: 'top',
+      },
+      grid: {
+        show: true,
+        top: '5%',
+        left: '5%',
+        right: '5%',
+        bottom: '5%',
+        containLabel: true,
+      },
+      tooltip: {
+        show: true,
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
+        },
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          saveAsImage: {},
+        },
+      },
+      xAxis: {
+        show: true,
+        type: 'category',
+        axisTick: {
+          alignWithLabel: true,
+        },
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        show: true,
+        type: 'value',
+      },
+      series: [
+        {
+          type: 'bar',
+          showBackground: true,
+          backgroundStyle: {
+            color: 'rgba(180, 180, 180, 0.2)',
+          },
+          data: [120, 200, 150, 80, 70, 110, 130],
+        },
+      ],
+    })
 })
 </script>
 
--- src/lib/chart.ts
+++ src/lib/chart.ts
@@ -1,10 +1,17 @@
 import * as echarts from 'echarts/core'
 import { BarChart, type BarSeriesOption } from 'echarts/charts'
-import { GridComponent, type GridComponentOption } from 'echarts/components'
+import { GridComponent, type GridComponentOption, TitleComponent, type TitleComponentOption, TooltipComponent, type TooltipComponentOption, ToolboxComponent, type ToolboxComponentOption, TransformComponent } from 'echarts/components'
+import { LabelLayout, UniversalTransition } from 'echarts/features'
 import { CanvasRenderer } from 'echarts/renderers'
 
-echarts.use([BarChart, GridComponent, CanvasRenderer])
+echarts.use([BarChart])
 
-export type EchartsOptions = echarts.ComposeOption<BarSeriesOption | GridComponentOption>
+echarts.use([GridComponent, TitleComponent, TooltipComponent, ToolboxComponent, TransformComponent])
+
+echarts.use([LabelLayout, UniversalTransition])
+
+echarts.use([CanvasRenderer])
+
+export type EchartsOptions = echarts.ComposeOption<BarSeriesOption | GridComponentOption | TitleComponentOption | TooltipComponentOption | ToolboxComponentOption>
 
 export default echarts

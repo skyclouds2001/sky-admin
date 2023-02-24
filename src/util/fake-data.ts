c869da8c48c00be4a28d3267182d6228e073f0cd
skyclouds2001 <skyclouds2001@163.com>
2023-02-25 01:41:11 +0800

    feat: add generate fake data method

---
+++ src/util/fake-data.ts
@@ -0,0 +1,26 @@
+/**
+ * 产生的名称方式
+ */
+const str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+/**
+ * 虚拟数据数据结构
+ */
+interface Data {
+  name: string
+  value: number
+}
+
+/**
+ * 生成虚拟数据方法
+ *
+ * @param len 待生成数据长度（小于 52）
+ * @param range 生成数据范围（为正整数）
+ * @returns 生成的虚拟数据
+ */
+export const generateFakeData = (len: number, range: number): Data[] => {
+  return new Array(len).fill(0).map((_, i) => ({
+    name: str.at(i) as string,
+    value: Math.random() * range,
+  }))
+}
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,5 @@
 export * from './open-page'
 
 export * from './generate-link'
+
+export * from './fake-data'

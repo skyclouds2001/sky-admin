8a17ef12efcd0a8c5551898671fbad495ea2f92a
skyclouds2001 <skyclouds2001@163.com>
2023-02-28 15:34:12 +0800

    feat: update bar-code page

--- src/views/feature/BarCode.vue
+++ src/views/feature/BarCode.vue
@@ -1,7 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { ElSpace, ElCard } from 'element-plus'
+import JsBarcode from 'jsbarcode'
+
+onMounted(() => {
+  JsBarcode('#simple-bar-code', '0123456789')
+  JsBarcode('#colorful-bar-code', '0123456789', {
+    lineColor: '#f00',
+  })
+  JsBarcode('#font-bar-code', '0123456789', {
+    font: 'fantasy',
+    fontOptions: 'bold italic',
+    fontSize: 30,
+  })
+})
+</script>
 
 <template>
-  <div>bar code</div>
+  <el-space size="large" class="w-full p-4">
+    <el-card shadow="always">
+      <canvas id="simple-bar-code"></canvas>
+    </el-card>
+    <el-card shadow="always">
+      <canvas id="colorful-bar-code"></canvas>
+    </el-card>
+    <el-card shadow="always">
+      <canvas id="font-bar-code"></canvas>
+    </el-card>
+  </el-space>
 </template>
 
 <style scoped lang="scss"></style>

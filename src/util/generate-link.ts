97c2d8a4542f93203dc95db62401a0adeb735322
skyclouds2001 <skyclouds2001@163.com>
2023-02-28 23:10:14 +0800

    feat: move generate fake-data method to util

--- src/util/generate-link.ts
+++ src/util/generate-link.ts
@@ -5,3 +5,11 @@
  * @returns - 依赖对应 npm 访问 URL
  */
 export const generateNpmLink = (name: string): string => `https://www.npmjs.com/package/${name}`
+
+/**
+ * 生成文档预览链接方法（使用 Microsoft Office 开放 API）
+ *
+ * @param url 原文档链接
+ * @returns 文档预览链接
+ */
+export const generateDocumentLink = (url: string): string => `https://view.officeapps.live.com/op/view.aspx?src=${url}`
--- src/views/docs/WordPreview.vue
+++ src/views/docs/WordPreview.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import { DEMO_WORD_SRC } from '@/config'
-
-const PREVIEW_WORD_SRC = `https://view.officeapps.live.com/op/view.aspx?src=${DEMO_WORD_SRC}`
+import { generateDocumentLink } from '@/util'
 </script>
 
 <template>
-  <iframe class="word-preview" name="word-preview" title="word-preview" referrerpolicy="no-referrer" :src="PREVIEW_WORD_SRC"></iframe>
+  <iframe class="word-preview" name="word-preview" title="word-preview" referrerpolicy="no-referrer" :src="generateDocumentLink(DEMO_WORD_SRC)"></iframe>
 </template>
 
 <style scoped lang="scss">
 .pdf-preview {
   width: 100%;
-  height: 100%;
   min-width: 1000px;
+  height: 100%;
   min-height: 1500px;
 }
 </style>

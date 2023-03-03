773fd168fbca5632c91c3632214dc0e2462ad8d6
skyclouds2001 <skyclouds2001@163.com>
2023-03-03 11:41:29 +0800

    feat: update router settings

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { pick } from 'lodash-es'
 import type { Tab, Page } from '@/model'
 import { useTabsStore, usePagesStore } from '@/store'
 import { openNewPageInNewTab } from '@/util'
@@ -9,152 +8,176 @@ const router = createRouter({
   routes: [
     // 首页
     {
-      name: '首页',
+      name: Symbol('/'),
       path: '/',
       component: () => import('@/views/HomePage.vue'),
+      meta: { title: '首页' },
     },
 
     // 图表
     {
-      name: '图表',
+      name: Symbol('/chart'),
       path: '/chart',
       redirect: '/chart/bar',
+      meta: { title: '图表' },
       children: [
         {
-          name: '柱状图',
+          name: Symbol('/chart/bar'),
           path: '/chart/bar',
           component: () => import('@/views/chart/BarChart.vue'),
+          meta: { title: '柱状图' },
         },
         {
-          name: '折线图',
+          name: Symbol('/chart/line'),
           path: '/chart/line',
           component: () => import('@/views/chart/LineChart.vue'),
+          meta: { title: '折线图' },
         },
         {
-          name: '饼图',
+          name: Symbol('/chart/pie'),
           path: '/chart/pie',
           component: () => import('@/views/chart/PieChart.vue'),
+          meta: { title: '饼图' },
         },
         {
-          name: '散点图',
+          name: Symbol('/chart/scatter'),
           path: '/chart/scatter',
           component: () => import('@/views/chart/ScatterChart.vue'),
+          meta: { title: '散点图' },
         },
       ],
     },
 
     // 嵌套菜单
     {
-      name: '嵌套菜单',
+      name: Symbol('/route'),
       path: '/route',
       redirect: '/route/1',
+      meta: { title: '嵌套菜单' },
       children: [
         {
-          name: '嵌套菜单-1',
+          name: Symbol('/route/1'),
           path: '/route/1',
           component: () => import('@/views/route/RouteFirst.vue'),
+          meta: { title: '嵌套菜单-1' },
         },
         {
-          name: '嵌套菜单-2',
+          name: Symbol('/route/2'),
           path: '/route/2',
           component: () => import('@/views/route/RouteSecond.vue'),
+          meta: { title: '嵌套菜单-2' },
           children: [
             {
-              name: '嵌套菜单-2-1',
+              name: Symbol('/route/2/1'),
               path: '/route/2/1',
               component: () => import('@/views/route/RouteSecondFirst.vue'),
+              meta: { title: '嵌套菜单-2-1' },
             },
             {
-              name: '嵌套菜单-2-2',
+              name: Symbol('/route/2/2'),
               path: '/route/2/2',
               component: () => import('@/views/route/RouteSecondSecond.vue'),
+              meta: { title: '嵌套菜单-2-2' },
               children: [
                 {
-                  name: '嵌套菜单-2-2-1',
+                  name: Symbol('/route/2/2/1'),
                   path: '/route/2/2/1',
                   component: () => import('@/views/route/RouteSecondSecondFirst.vue'),
+                  meta: { title: '嵌套菜单-2-2-1' },
                 },
                 {
-                  name: '嵌套菜单-2-2-2',
+                  name: Symbol('/route/2/2/2'),
                   path: '/route/2/2/2',
                   component: () => import('@/views/route/RouteSecondSecondSecond.vue'),
+                  meta: { title: '嵌套菜单-2-2-2' },
                 },
                 {
-                  name: '嵌套菜单-2-2-3',
+                  name: Symbol('/route/2/2/3'),
                   path: '/route/2/2/3',
                   component: () => import('@/views/route/RouteSecondSecondThird.vue'),
+                  meta: { title: '嵌套菜单-2-2-3' },
                 },
               ],
             },
             {
-              name: '嵌套菜单-2-3',
+              name: Symbol('/route/2/3'),
               path: '/route/2/3',
               component: () => import('@/views/route/RouteSecondThird.vue'),
+              meta: { title: '嵌套菜单-2-3' },
             },
           ],
         },
         {
-          name: '嵌套菜单-3',
+          name: Symbol('/route/3'),
           path: '/route/3',
           component: () => import('@/views/route/RouteThird.vue'),
+          meta: { title: '嵌套菜单-3' },
         },
       ],
     },
 
     // 文档
     {
-      name: '文档',
+      name: Symbol('/docs'),
       path: '/docs',
       redirect: '/docs/word-preview',
+      meta: { title: '文档' },
       children: [
         {
-          name: 'Word 预览',
+          name: Symbol('/docs/word-preview'),
           path: '/docs/word-preview',
           component: () => import('@/views/docs/WordPreview.vue'),
+          meta: { title: 'Word 预览' },
         },
         {
-          name: 'Excel 预览',
+          name: Symbol('/docs/excel-preview'),
           path: '/docs/excel-preview',
           component: () => import('@/views/docs/ExcelPreview.vue'),
+          meta: { title: 'Excel 预览' },
         },
         {
-          name: 'PPT 预览',
+          name: Symbol('/docs/ppt-preview'),
           path: '/docs/ppt-preview',
           component: () => import('@/views/docs/PPTPreview.vue'),
+          meta: { title: 'PPT 预览' },
         },
         {
-          name: 'PDF 预览',
+          name: Symbol('/docs/pdf-preview'),
           path: '/docs/pdf-preview',
           component: () => import('@/views/docs/PDFPreview.vue'),
+          meta: { title: 'PDF 预览' },
         },
       ],
     },
 
     // 功能
     {
-      name: '功能',
+      name: Symbol('/feature'),
       path: '/feature',
       redirect: '/feature/bar-code',
+      meta: { title: '功能' },
       children: [
         {
-          name: '条形码',
+          name: Symbol('/feature/bar-code'),
           path: '/feature/bar-code',
           component: () => import('@/views/feature/BarCode.vue'),
+          meta: { title: '条形码' },
         },
         {
-          name: '二维码',
+          name: Symbol('/feature/qrcode'),
           path: '/feature/qrcode',
           component: () => import('@/views/feature/QrCode.vue'),
+          meta: { title: '二维码' },
         },
       ],
     },
 
     // 关于
     {
-      name: '关于',
+      name: Symbol('/about'),
       path: '/about',
       component: () => import('@/views/AboutPage.vue'),
-      meta: { title: '' },
+      meta: { title: '关于' },
     },
   ],
   scrollBehavior(to, from, savedPosition) {
@@ -180,7 +203,11 @@ router.beforeEach((to) => {
   const tabsStore = useTabsStore()
   tabsStore.$patch((state) => {
     if (tabsStore.tabs.findIndex((v) => v.path === to.path) === -1) {
-      state.tabs.push(pick(to, ['path', 'name']) as Tab)
+      const tab: Tab = {
+        name: to.meta.title as string,
+        path: to.path,
+      }
+      state.tabs.push(tab)
     }
     state.currentTab = to.path
   })
@@ -188,7 +215,13 @@ router.beforeEach((to) => {
   // 自动更新 pagesStore 内的 pages 数据
   const pagesStore = usePagesStore()
   pagesStore.$patch((state) => {
-    state.pages = to.matched.map((v) => pick(v, ['path', 'name']) as Page)
+    state.pages = to.matched.map((v) => {
+      const page: Page = {
+        name: v.meta.title as string,
+        path: v.path,
+      }
+      return page
+    })
     if (to.path !== '/') {
       state.pages.unshift({
         name: '首页',

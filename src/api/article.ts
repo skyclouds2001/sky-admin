bb4cebe3f899bc38bd1eecf216996e37693d999f
skyclouds2001 <skyclouds2001@163.com>
2023-03-06 12:59:51 +0800

    feat: update get articles list api

--- src/api/article.ts
+++ src/api/article.ts
@@ -4,6 +4,9 @@ import type { Response, Article } from '@/model'
 /**
  * 获取文章列表方法
  *
+ * @param data 分页参数
+ * @param data.page 页面下标
+ * @param data.size 页面容量
  * @returns 文章列表数据
  */
-export const getArticles = (): Promise<Response<{ articles: Article[] }>> => http.get('/api/article').then((res) => res.data)
+export const getArticles = ({ page, size }: { page: number; size: number }): Promise<Response<{ articles: Article[] }>> => http.get(`/api/article?page=${page}&size=${size}`).then((res) => res.data)

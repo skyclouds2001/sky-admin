aaf6ae0918a70c9655c9e77a6686159261b66b15
skyclouds2001 <skyclouds2001@163.com>
2023-03-08 19:06:09 +0800

    feat: update useArticle hook

--- src/hook/useArticles.ts
+++ src/hook/useArticles.ts
@@ -1,4 +1,5 @@
 import { ref, watch, type Ref } from 'vue'
+import { ElMessage } from 'element-plus'
 import { getArticles } from '@/api'
 import type { Article } from '@/model'
 
@@ -7,6 +8,7 @@ const useArticles = (): {
   total: Ref<number>
   page: Ref<number>
   size: Ref<number>
+  loading: Ref<boolean>
 } => {
   const articles = ref<Article[]>([])
 
@@ -16,9 +18,12 @@ const useArticles = (): {
 
   const size = ref(10)
 
+  const loading = ref(false)
+
   watch(
     [page, size],
     async ([currentPage, currentSize]) => {
+      loading.value = true
       try {
         const res = await getArticles({
           page: currentPage,
@@ -28,10 +33,17 @@ const useArticles = (): {
           articles.value = res.data.articles
           total.value = res.data.total
         } else {
-          console.error(res.code, res.message)
+          ElMessage.error({
+            message: `ERROR ${res.code}: ${res.message}`,
+            showClose: true,
+            center: true,
+            grouping: true,
+          })
         }
       } catch (error) {
         console.error(error)
+      } finally {
+        loading.value = false
       }
     },
     {
@@ -44,6 +56,7 @@ const useArticles = (): {
     total,
     page,
     size,
+    loading,
   }
 }
 

896b7c5d7c7943f9e368e2c232a3ceb96c21836a
skyclouds2001 <skyclouds2001@163.com>
2023-03-11 03:29:20 +0800

    feat: add base64 encoder support

--- src/views/feature/EncodeDecode.vue
+++ src/views/feature/EncodeDecode.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElSpace } from 'element-plus'
+import Base64Encoder from '@/components/feature/Base64Encoder.vue'
+</script>
 
 <template>
-  <div>encode decode</div>
+  <el-space direction="vertical" fill size="large" class="px-10 py-4">
+    <base64-encoder />
+  </el-space>
 </template>
 
 <style scoped lang="scss"></style>

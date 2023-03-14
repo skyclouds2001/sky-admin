1b323fed7a34c520d678f6abdc48609d8e6a5488
skyclouds2001 <skyclouds2001@163.com>
2023-03-14 19:32:11 +0800

    fix: update toggle dayjs

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,14 @@ import { ElConfigProvider } from 'element-plus'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import en from 'element-plus/dist/locale/en.mjs'
 import 'element-plus/theme-chalk/dark/css-vars.css'
-import dayjs from 'dayjs'
+import { locale } from 'dayjs'
 import 'dayjs/locale/zh-cn'
+import 'dayjs/locale/en'
 import LayoutContainer from '@/layout/LayoutContainer.vue'
 
 const i18n = useI18n()
 
-dayjs.locale(i18n.locale.value === 'zh-CN' ? 'zh-cn' : 'en')
+locale(i18n.locale.value === 'zh-CN' ? 'zh-cn' : 'en')
 </script>
 
 <template>

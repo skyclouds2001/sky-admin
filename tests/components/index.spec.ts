3c684171b48397e2c042f312d937d8f2986c4d04
skyclouds2001 <skyclouds2001@163.com>
2023-03-25 22:35:01 +0800

    test: update home page test

--- tests/components/index.spec.ts
+++ tests/components/index.spec.ts
@@ -1,10 +1,17 @@
 import { describe, it } from 'vitest'
 import { render, screen } from '@testing-library/vue'
+import router from '@/router'
+import store from '@/store'
+import i18n from '@/i18n'
 import HomePage from '@/views/HomePage.vue'
 
-describe('basic tests', () => {
-  it('basic test', () => {
-    render(HomePage)
-    screen.getByText('home')
+describe('HomePage', () => {
+  it('should has model part title', () => {
+    render(HomePage, {
+      global: {
+        plugins: [router, store, i18n],
+      },
+    })
+    screen.getByText('作者信息')
   })
 })

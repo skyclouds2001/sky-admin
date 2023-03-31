591f565473b039b1022d37970334b8447335fdbd
skyclouds2001 <skyclouds2001@163.com>
2023-03-31 23:24:49 +0800

    feat: update webRTC base page

--- src/views/feature/WebRTC.vue
+++ src/views/feature/WebRTC.vue
@@ -1,7 +1,109 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { ElButton, ElCard, ElForm, ElFormItem, ElInput, ElSpace } from 'element-plus'
+
+const pc = new RTCPeerConnection()
+
+/**
+ * 初始化
+ */
+const handleInitWebRTC = async () => {
+  const local = document.getElementById('local') as HTMLVideoElement
+  const remote = document.getElementById('remote') as HTMLVideoElement
+
+  const remoteStream = new MediaStream()
+
+  pc.addEventListener('track', (e) => {
+    e.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track)
+    })
+    remote.srcObject = remoteStream
+  })
+
+  const localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  })
+
+  local.srcObject = localStream
+
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream)
+  })
+}
+
+const offerSdp = ref('')
+const answerSdp = ref('')
+
+const createOffer = async () => {
+  const offer = await pc.createOffer()
+  await pc.setLocalDescription(offer)
+
+  pc.addEventListener('icecandidate', (e) => {
+    if (e.candidate) {
+      offerSdp.value = JSON.stringify(pc.localDescription)
+    }
+  })
+}
+
+const createAnswer = async () => {
+  const offer = JSON.parse(offerSdp.value)
+
+  pc.addEventListener('icecandidate', (e) => {
+    if (e.candidate) {
+      answerSdp.value = JSON.stringify(pc.localDescription)
+    }
+  })
+
+  await pc.setRemoteDescription(offer)
+
+  const answer = await pc.createAnswer()
+  await pc.setLocalDescription(answer)
+}
+
+const addAnswer = () => {
+  const answer = JSON.parse(answerSdp.value)
+  if (!pc.currentRemoteDescription) {
+    pc.setRemoteDescription(answer)
+  }
+}
+</script>
 
 <template>
-  <div>web-r-t-c</div>
+  <el-space size="large" class="w-full p-5">
+    <el-space size="large" direction="vertical">
+      <video id="local" ref="el" width="800" height="600" autoplay playsinline muted />
+      <video id="remote" ref="el" width="800" height="600" autoplay playsinline muted />
+    </el-space>
+    <el-space size="large" direction="vertical">
+      <el-button type="primary" @click="handleInitWebRTC">初始化</el-button>
+      <el-card header="用户A">
+        <el-form>
+          <el-form-item>
+            <el-button type="primary" @click="createOffer">创建Offer</el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-input v-model="offerSdp" />
+          </el-form-item>
+        </el-form>
+      </el-card>
+      <el-card header="用户B">
+        <el-form>
+          <el-form-item>
+            <el-button type="primary" @click="createAnswer">创建Answer</el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-input v-model="answerSdp" />
+          </el-form-item>
+        </el-form>
+      </el-card>
+      <el-card header="用户A">
+        <el-form-item>
+          <el-button type="primary" @click="addAnswer">添加Answer</el-button>
+        </el-form-item>
+      </el-card>
+    </el-space>
+  </el-space>
 </template>
 
 <style scoped></style>

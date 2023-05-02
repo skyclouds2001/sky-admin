23f703e2317a056a973f502e79189ef38ddd1d44
skyclouds2001 <skyclouds2001@163.com>
2023-05-02 20:21:42 +0800

    perf: remove useless code

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,7 +9,3 @@ export { default as usePagesStore } from './pages'
 export { default as useTabsStore } from './tabs'
 
 export { default as useMenuStore } from './menu'
-
-export { default as useThemeStore } from './theme'
-
-export { default as useLangStore } from './lang'

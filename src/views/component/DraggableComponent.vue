3855a7dc930c3f3cd63a04437c1a9b5019ec9f30
skyclouds2001 <skyclouds2001@163.com>
2023-05-02 20:19:04 +0800

    feat: add draggable component

--- src/views/component/DraggableComponent.vue
+++ src/views/component/DraggableComponent.vue
@@ -1,7 +1,66 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount, reactive, ref } from 'vue'
+
+const el = ref<HTMLDivElement | null>(null)
+
+const isActive = ref(false)
+
+const position = reactive({
+  top: 40,
+  left: -40,
+})
+
+const handleMouseDown = () => {
+  if (!isActive.value) {
+    isActive.value = true
+  }
+}
+
+const handleMouseMove = (e: MouseEvent) => {
+  if (isActive.value) {
+    position.left = position.left + e.movementX
+    position.top = position.top + e.movementY
+  }
+}
+
+const handleMouseUp = () => {
+  if (isActive.value) {
+    isActive.value = false
+  }
+}
+
+onMounted(() => {
+  el.value?.addEventListener('mousedown', handleMouseDown)
+  el.value?.addEventListener('mousemove', handleMouseMove)
+  el.value?.addEventListener('mouseup', handleMouseUp)
+})
+
+onBeforeUnmount(() => {
+  el.value?.removeEventListener('mousedown', handleMouseDown)
+  el.value?.removeEventListener('mousemove', handleMouseMove)
+  el.value?.removeEventListener('mouseup', handleMouseUp)
+})
+</script>
 
 <template>
-  <div>draggable</div>
+  <div class="draggable-box">
+    <div ref="el" :class="['draggable-item', isActive ? 'active' : '']" :style="{ top: `${position.top}px`, left: `${position.left}px` }">draggable component</div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.draggable-box {
+  @apply relative;
+
+  .draggable-item {
+    @apply w-40 h-40;
+    @apply absolute z-[999] top-10 -left-10;
+    @apply shadow-xl;
+    @apply cursor-grab;
+
+    &.active {
+      @apply cursor-grabbing;
+    }
+  }
+}
+</style>

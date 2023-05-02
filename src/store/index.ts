64851b609759a1299315b2ac6d4feaba0a5abefd
skyclouds2001 <skyclouds2001@163.com>
2023-05-02 20:20:00 +0800

    style: rename pinia to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
 import { createPinia } from 'pinia'
 
-const pinia = createPinia()
+const store = createPinia()
 
-export default pinia
+export default store
 
 export { default as usePagesStore } from './pages'
 

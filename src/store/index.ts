ef26dd684187c0580f14f5389f8ac2a124b86c43
skyclouds2001 <skyclouds2001@163.com>
2023-05-04 23:40:15 +0800

    style: rename files

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,8 @@ const store = createPinia()
 
 export default store
 
-export { default as usePagesStore } from './pages'
+export { default as usePagesStore } from './page'
 
-export { default as useTabsStore } from './tabs'
+export { default as useTabsStore } from './tab'
 
 export { default as useMenuStore } from './menu'

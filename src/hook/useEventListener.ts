ed348f8412bf578dd6ae6509bf2ec2d4c096d74d
skyclouds2001 <skyclouds2001@163.com>
2023-05-06 13:03:03 +0800

    feat: update useEventListener hook

--- src/hook/useEventListener.ts
+++ src/hook/useEventListener.ts
@@ -1,11 +1,12 @@
 import { onMounted, onBeforeUnmount } from 'vue'
 
-// @ts-expect-error todo
-const useEventListener = <ET extends EventTarget = EventTarget, T extends string = ET extends Window ? keyof WindowEventMap : ET extends Document ? keyof DocumentEventMap : string, E extends Event = T extends keyof WindowEventMap ? WindowEventMap[T] : T extends keyof DocumentEventMap ? DocumentEventMap[T] : Event, ECB extends EventListener = (this: ET, evt: E) => void>(target: ET, type: T, callback: ECB, options?: AddEventListenerOptions | EventListenerOptions | boolean): void => {
+const useEventListener = <ET extends EventTarget = EventTarget, T extends string = ET extends Window ? keyof WindowEventMap : ET extends Document ? keyof DocumentEventMap : string, E extends Event = ET extends Window ? (T extends keyof WindowEventMap ? WindowEventMap[T] : never) : ET extends Document ? (T extends keyof DocumentEventMap ? DocumentEventMap[T] : never) : Event>(target: ET, type: T, callback: (this: ET, evt: E) => void, options?: AddEventListenerOptions | EventListenerOptions | boolean): void => {
   onMounted(() => {
+    // @ts-expect-error todo
     target.addEventListener(type, callback, options)
   })
   onBeforeUnmount(() => {
+    // @ts-expect-error todo
     target.removeEventListener(type, callback, options)
   })
 }

787be5057772c9f1c8e87d1c6a791cf520ab7a40
skyclouds2001 <skyclouds2001@163.com>
2023-05-07 19:29:02 +0800

    feat: update of using scope method

--- src/hook/useEventListener.ts
+++ src/hook/useEventListener.ts
@@ -1,14 +1,14 @@
-import { onMounted, onBeforeUnmount } from 'vue'
+import { getCurrentScope, onScopeDispose } from 'vue'
 
 const useEventListener = <ET extends EventTarget = EventTarget, T extends string = ET extends Window ? keyof WindowEventMap : ET extends Document ? keyof DocumentEventMap : string, E extends Event = ET extends Window ? (T extends keyof WindowEventMap ? WindowEventMap[T] : never) : ET extends Document ? (T extends keyof DocumentEventMap ? DocumentEventMap[T] : never) : Event>(target: ET, type: T, callback: (this: ET, evt: E) => void, options?: AddEventListenerOptions | EventListenerOptions | boolean): void => {
-  onMounted(() => {
+  if (getCurrentScope() !== undefined) {
     // @ts-expect-error todo
     target.addEventListener(type, callback, options)
-  })
-  onBeforeUnmount(() => {
-    // @ts-expect-error todo
-    target.removeEventListener(type, callback, options)
-  })
+    onScopeDispose(() => {
+      // @ts-expect-error todo
+      target.removeEventListener(type, callback, options)
+    })
+  }
 }
 
 export default useEventListener

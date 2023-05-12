c52b4e63cff0cfc42f7b2cf8d0363c8d0b2466ab
skyclouds2001 <skyclouds2001@163.com>
2023-05-12 09:47:17 +0800

    feat: add type enum

--- src/enum/index.ts
+++ src/enum/index.ts
@@ -1,3 +1,5 @@
 export { default as Lang } from './lang'
 
 export { default as Theme } from './theme'
+
+export { default as Type } from './type'
---
+++ src/enum/type.ts
@@ -0,0 +1,16 @@
+enum Type {
+  number = 'number',
+  string = 'string',
+  boolean = 'boolean',
+  object = ' object',
+  null = 'null',
+  undefined = 'undefined',
+  symbol = 'symbol',
+  bigint = 'bigint',
+  function = 'function',
+  date = 'date',
+  map = 'map',
+  set = 'set',
+}
+
+export default Type

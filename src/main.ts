7736f54adbcea8af5f4e06dc02e8c630c5f76b55
skyclouds2001 <skyclouds2001@163.com>
2023-05-16 13:49:49 +0800

    style: reformat

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import '@/style/global.css'
-import '@/style/mode.scss'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 import i18n from '@/i18n'
+import '@/style/global.css'
+import '@/style/mode.scss'
 
 createApp(App).use(router).use(store).use(i18n).mount('#app')

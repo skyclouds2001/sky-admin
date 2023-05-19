5a686f06106fb7979ccdfd208f9daeccf1e70a01
skyclouds2001 <skyclouds2001@163.com>
2023-05-19 15:30:41 +0800

    feat: add support flag for usePermission hook

--- src/hook/usePermission.ts
+++ src/hook/usePermission.ts
@@ -1,7 +1,14 @@
 import { computed, type ComputedRef, ref } from 'vue'
 import { useEventListener } from '@/hook'
 
-const usePermission = (name: PermissionName): ComputedRef<PermissionState | null> => {
+const usePermission = (
+  name: PermissionName
+): {
+  isSupported: boolean
+  status: ComputedRef<PermissionState | null>
+} => {
+  const isSupported = 'permissions' in navigator
+
   const permission = ref<PermissionState | null>(null)
 
   const update = (): void => {
@@ -13,12 +20,15 @@ const usePermission = (name: PermissionName): ComputedRef<PermissionState | null
   void navigator.permissions.query({ name }).then((permissionStatus) => {
     permission.value = permissionStatus.state
 
-    useEventListener<PermissionStatus, PermissionStatusEventMap>(permissionStatus, 'change', () => {
+    useEventListener<PermissionStatus, PermissionStatusEventMap, 'change'>(permissionStatus, 'change', () => {
       update()
     })
   })
 
-  return computed(() => permission.value)
+  return {
+    isSupported,
+    status: computed(() => permission.value),
+  }
 }
 
 export default usePermission

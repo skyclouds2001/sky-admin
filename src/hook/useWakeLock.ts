908552b628d0612f20d38c8144a783fa5f31464e
skyclouds2001 <skyclouds2001@163.com>
2023-05-24 12:04:38 +0800

    fix: add wakelock type override

--- src/hook/useWakeLock.ts
+++ src/hook/useWakeLock.ts
@@ -1,6 +1,32 @@
 import { computed, type ComputedRef, ref } from 'vue'
 import { useEventListener } from '@/hook'
 
+interface NavigatorWithWakeLock extends Navigator {
+  readonly wakeLock: WakeLock
+}
+
+interface WakeLock {
+  request: (type?: WakeLockType) => Promise<WakeLockSentinel>
+}
+
+type WakeLockType = 'screen'
+
+interface WakeLockSentinel extends EventTarget {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onrelease: ((this: WakeLockSentinel, ev: Event) => any) | null
+  readonly released: boolean
+  readonly type: WakeLockType
+  release: () => Promise<void>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  addEventListener: (<K extends keyof WakeLockSentinelEventMap>(type: K, listener: (this: WakeLockSentinel, ev: WakeLockSentinelEventMap[K]) => any, options?: boolean | AddEventListenerOptions) => void) & ((type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions) => void)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  removeEventListener: (<K extends keyof WakeLockSentinelEventMap>(type: K, listener: (this: WakeLockSentinel, ev: WakeLockSentinelEventMap[K]) => any, options?: boolean | EventListenerOptions) => void) & ((type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions) => void)
+}
+
+interface WakeLockSentinelEventMap {
+  release: Event
+}
+
 const useWakeLock = (): {
   isSupported: boolean
   isActive: ComputedRef<boolean>
@@ -17,10 +43,10 @@ const useWakeLock = (): {
   const request = async (): Promise<void> => {
     if (!isSupported) return
 
-    const wakeLock = await navigator.wakeLock.request('screen')
+    const wakeLock = await (navigator as NavigatorWithWakeLock).wakeLock.request('screen')
 
     wakeLockSentinel = wakeLock
-    isActive.value = wakeLockSentinel.released === false
+    isActive.value = !wakeLockSentinel.released
   }
 
   const release = async (): Promise<void> => {
@@ -30,7 +56,7 @@ const useWakeLock = (): {
 
     await wakeLockSentinel.release()
 
-    isActive.value = wakeLockSentinel !== null ? wakeLockSentinel.released === false : false
+    isActive.value = wakeLockSentinel !== null ? !wakeLockSentinel.released : false
     wakeLockSentinel = null
   }
 
@@ -45,8 +71,8 @@ const useWakeLock = (): {
 
     if (document.visibilityState === 'hidden') return
 
-    void navigator.wakeLock.request('screen').then((wakeLock) => {
-      isActive.value = wakeLock.released === false
+    void (navigator as NavigatorWithWakeLock).wakeLock.request('screen').then((wakeLock) => {
+      isActive.value = wakeLock.released
     })
   })
 

8d615d92f41cb83375f93d8b48337df1242f79b5
skyclouds2001 <skyclouds2001@163.com>
2023-05-31 15:54:24 +0800

    feat: update useDeviceList hook

--- src/hook/useDevicesList.ts
+++ src/hook/useDevicesList.ts
@@ -1,13 +1,19 @@
 import { computed, type ComputedRef, shallowRef, type ShallowRef } from 'vue'
-import { useEventListener } from '@/hook'
+import { useEventListener, usePermission } from '@/hook'
 
-const useDevicesList = (): {
+const useDevicesList = (
+  options: {
+    requestImmediate?: boolean
+  } = {}
+): {
   isSupported: boolean
   devices: ShallowRef<MediaDeviceInfo[]>
   videoInputs: ComputedRef<MediaDeviceInfo[]>
   audioInputs: ComputedRef<MediaDeviceInfo[]>
   audioOutputs: ComputedRef<MediaDeviceInfo[]>
 } => {
+  const { requestImmediate = false } = options
+
   const isSupported = 'mediaDevices' in navigator && 'enumerateDevices' in navigator.mediaDevices
 
   const devices = shallowRef<MediaDeviceInfo[]>([])
@@ -16,14 +22,37 @@ const useDevicesList = (): {
   const audioInputs = computed(() => devices.value?.filter((device) => device.kind === 'audioinput'))
   const audioOutputs = computed(() => devices.value?.filter((device) => device.kind === 'audiooutput'))
 
+  const { status } = usePermission('camera')
+
   const update = async (): Promise<void> => {
+    if (!isSupported) return
+
+    if (status.value !== 'granted') {
+      await requestPermission()
+    }
+
     devices.value = await navigator.mediaDevices.enumerateDevices()
+  }
+
+  const requestPermission = async (): Promise<void> => {
+    if (!isSupported) return
+
+    if (status.value === 'granted') return
+
+    await navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+
+    void update()
   }
 
   if (isSupported) {
     useEventListener<MediaDevices, MediaDevicesEventMap, 'devicechange'>(navigator.mediaDevices, 'devicechange', () => {
       void update()
     })
+
+    if (requestImmediate) {
+      void requestPermission()
+    }
+
     void update()
   }
 

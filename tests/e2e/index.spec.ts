8944cbc3ccd96090fccc8d95799de03f8e20875a
skyclouds2001 <skyclouds2001@163.com>
2023-06-03 17:17:44 +0800

    test: update e2e test

--- tests/e2e/index.spec.ts
+++ tests/e2e/index.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
+  await page.goto('https://localhost:5173/')
 
   await expect(page).toHaveTitle('SkyAdmin')
+
+  await expect(page).toHaveURL('https://localhost:5173/')
 })

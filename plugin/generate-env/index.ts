445771fd63964366a90ca800357ac098afdf64b6
skyclouds2001 <skyclouds2001@163.com>
2023-06-05 15:35:56 +0800

    feat: also generate mode env and node_env env

--- plugin/generate-env/index.ts
+++ plugin/generate-env/index.ts
@@ -7,6 +7,8 @@ const GenerateEnv = (): Plugin => {
     config: () => ({
       define: {
         __BUILD_TIME__: `'${dayjs().format('YYYY-MM-DD HH:mm:ss')}'`,
+        __MODE__: process.env.NODE_ENV,
+        __NODE_ENV__: process.env.NODE_ENV,
       },
     }),
     enforce: 'pre',

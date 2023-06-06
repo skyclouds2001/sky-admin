446aef836fa6f8040fd3be1aefa1a63bbed215b2
skyclouds2001 <skyclouds2001@163.com>
2023-06-06 10:50:38 +0800

    feat: add useWindowControlsOverlay hook

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -96,6 +96,8 @@ export { default as useVibrate } from './useVibrate'
 
 export { default as useWakeLock } from './useWakeLock'
 
+export { default as useWindowControlsOverlay } from './useWindowControlsOverlay'
+
 export { default as useWindowFocus } from './useWindowFocus'
 
 export { default as useWindowScroll } from './useWindowScroll'
---
+++ src/hook/useWindowControlsOverlay.ts
@@ -0,0 +1,48 @@
+import { computed, type ComputedRef, ref, shallowRef } from 'vue'
+import { useEventListener } from '@/hook'
+
+interface NavigatorWithWindowControlsOverlay extends Navigator {
+  readonly windowControlsOverlay: WindowControlsOverlay
+}
+
+interface WindowControlsOverlay extends EventTarget {
+  readonly visible: boolean
+  getTitlebarAreaRect: () => DOMRect
+  ongeometrychange: ((this: WindowControlsOverlay, ev: WindowControlsOverlayGeometryChangeEvent) => void) | null
+  addEventListener: (<K extends keyof WindowControlsOverlayEventMap>(type: K, listener: (this: WindowControlsOverlay, ev: WindowControlsOverlayEventMap[K]) => void, options?: boolean | AddEventListenerOptions) => void) & ((type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions) => void)
+  removeEventListener: (<K extends keyof WindowControlsOverlayEventMap>(type: K, listener: (this: WindowControlsOverlay, ev: WindowControlsOverlayEventMap[K]) => void, options?: boolean | EventListenerOptions) => void) & ((type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions) => void)
+}
+
+interface WindowControlsOverlayEventMap {
+  geometrychange: WindowControlsOverlayGeometryChangeEvent
+}
+
+interface WindowControlsOverlayGeometryChangeEvent extends Event {
+  readonly titlebarAreaRect: DOMRect
+  readonly visible: boolean
+}
+
+const useWindowControlsOverlay = (): {
+  isSupported: boolean
+  visible: ComputedRef<boolean>
+  rect: ComputedRef<DOMRect>
+} => {
+  const isSupported = 'windowControlsOverlay' in navigator
+
+  const visible = ref((navigator as NavigatorWithWindowControlsOverlay).windowControlsOverlay.visible)
+
+  const rect = shallowRef((navigator as NavigatorWithWindowControlsOverlay).windowControlsOverlay.getTitlebarAreaRect())
+
+  useEventListener<WindowControlsOverlay, WindowControlsOverlayEventMap, 'geometrychange'>((navigator as NavigatorWithWindowControlsOverlay).windowControlsOverlay, 'geometrychange', (e) => {
+    visible.value = e.visible
+    rect.value = e.titlebarAreaRect
+  })
+
+  return {
+    isSupported,
+    visible: computed(() => visible.value),
+    rect: computed(() => rect.value),
+  }
+}
+
+export default useWindowControlsOverlay

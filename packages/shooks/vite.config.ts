a0875ee14a3ead1972e199cefae8bf2621d959af
skyclouds2001 <skyclouds2001@163.com>
2023-06-14 23:04:27 +0800

    build: add shooks build support

---
+++ packages/shooks/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import path from 'node:path'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      formats: ['es', 'cjs', 'umd', 'iife'],
+      name: 'shooks',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})

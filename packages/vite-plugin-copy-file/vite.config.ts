c6154f42d4b63adf80dc698e37020ca4b87e6576
skyclouds2001 <skyclouds2001@163.com>
2023-06-16 23:23:35 +0800

    build: update package build script

--- packages/vite-plugin-copy-file/vite.config.ts
+++ packages/vite-plugin-copy-file/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'index.ts'),

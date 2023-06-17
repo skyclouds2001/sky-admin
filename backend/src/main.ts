1406559c2c991b71782b7b6a14e8c6e9e4fef1f9
skyclouds2001 <skyclouds2001@163.com>
2023-06-17 15:33:42 +0800

    feat: add global exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core'
+import { HttpAdapterHost, NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter'
 
 /**
  * 初始化方法
@@ -12,6 +13,8 @@ async function bootstrap(): Promise<void> {
   SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, new DocumentBuilder().setTitle('Sky Admin').setDescription('Api route document for Sky Admin').setVersion('0.0.0').build()))
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+  app.useGlobalFilters(new PrismaClientExceptionFilter(app.get(HttpAdapterHost).httpAdapter))
 
   await app.listen(3000)
 }

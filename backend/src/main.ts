8fc613c85f4c547010c03caa029e0c66e1573cb7
skyclouds2001 <skyclouds2001@163.com>
2023-06-17 12:06:18 +0800

    feat: add swagger tool

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 /**
  * 初始化方法
  */
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
+
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, new DocumentBuilder().setTitle('Sky Admin').setDescription('Api route document for Sky Admin').setVersion('0.0.0').build()))
+
   await app.listen(3000)
 }
 

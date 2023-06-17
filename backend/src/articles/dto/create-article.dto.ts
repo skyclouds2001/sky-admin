55e677b4378a34103c0a09c198415b2ee6dccc9e
skyclouds2001 <skyclouds2001@163.com>
2023-06-17 14:03:49 +0800

    feat: update validation

--- backend/src/articles/dto/create-article.dto.ts
+++ backend/src/articles/dto/create-article.dto.ts
@@ -1,25 +1,47 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { ArticleStatus } from '@prisma/client'
+import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, Max, MaxLength, Min, MinLength } from 'class-validator'
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   @ApiProperty()
   title: string
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(250)
   @ApiProperty({ required: false })
   description?: string
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   author: string
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   reviewer: string
 
+  @IsInt()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(5)
   @ApiProperty()
   star: number
 
+  @IsInt()
+  @IsNotEmpty()
+  @IsPositive()
   @ApiProperty()
   read: number
 
+  @IsEnum(ArticleStatus)
+  @IsOptional()
+  @IsNotEmpty()
   @ApiProperty({ required: false, default: 'Drafted', enum: [ArticleStatus.Drafted, ArticleStatus.Published] })
-  status: ArticleStatus
+  status: ArticleStatus = 'Drafted'
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 /**
  * 初始化方法
@@ -9,6 +10,8 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
 
   SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, new DocumentBuilder().setTitle('Sky Admin').setDescription('Api route document for Sky Admin').setVersion('0.0.0').build()))
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
   await app.listen(3000)
 }

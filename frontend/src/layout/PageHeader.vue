eb4ccc9ebc209dcb416960d3a46e5bce9f50aa04
skyclouds2001 <skyclouds2001@163.com>
2023-06-25 10:58:29 +0800

    feat: add exit login support

--- frontend/src/layout/PageHeader.vue
+++ frontend/src/layout/PageHeader.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import { inject, type Ref } from 'vue'
+import { getCurrentInstance, inject, type Ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
-import { ElBreadcrumb, ElBreadcrumbItem, ElImage, ElIcon } from 'element-plus'
+import { ElBreadcrumb, ElBreadcrumbItem, ElDropdown, ElDropdownMenu, ElDropdownItem, ElImage, ElIcon, ElMessageBox, ElMessage } from 'element-plus'
 import { FullScreen, Lock, Setting, Share, Unlock } from '@element-plus/icons-vue'
+import { useFullscreen, useNow, usePointerLock, useShare, useStorage } from 'shooks'
 import { PROJECT_AUTHOR_NAME, PROJECT_AUTHOR_AVATAR } from '@/config'
-import { useFullscreen, useNow, usePointerLock, useShare } from 'shooks'
 import { usePagesStore } from '@/store'
+
+const appContext = getCurrentInstance()?.appContext
+
+const router = useRouter()
 
 const i18n = useI18n()
 
@@ -18,6 +23,10 @@ const now = useNow()
 const { isSupported: isSupportedPointerLock, isPointerLock, trigger } = usePointerLock()
 
 const { isSupported: isSupportedShare, share } = useShare()
+
+const storage = useStorage<string>('token', {
+  prefix: 'sky-admin-0.0.0',
+})
 
 const isShowSettingDrawer = inject<Ref<boolean>>('setting')
 
@@ -38,6 +47,38 @@ const handleShare = () => {
     title: document.title,
     url: location.href,
   })
+}
+
+/**
+ * 退出登录
+ */
+const exitLogin = () => {
+  ElMessageBox.confirm(
+    '确认退出登录？',
+    '警告',
+    {
+      distinguishCancelAndClose: true,
+      type: 'info',
+      confirmButtonText: '确认',
+      cancelButtonText: '取消',
+    },
+    appContext
+  )
+    .then(() => {
+      storage.value = null
+      router.push('/login')
+    })
+    .catch(() => {
+      ElMessage.info(
+        {
+          message: '取消退出登录',
+          showClose: true,
+          center: true,
+          grouping: true,
+        },
+        appContext
+      )
+    })
 }
 </script>
 
@@ -61,12 +102,21 @@ const handleShare = () => {
       <div v-if="isSupportedShare" class="share" @click="handleShare">
         <el-icon :size="20"><Share /></el-icon>
       </div>
+      <el-dropdown trigger="click">
+        <div class="user">
+          <div class="username">{{ PROJECT_AUTHOR_NAME }}</div>
+          <div class="avatar">
+            <el-image :src="PROJECT_AUTHOR_AVATAR" fit="cover" loading="lazy" lazy alt="avatar" />
+          </div>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item @click="exitLogin">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
       <div class="settings" @click="showSettingDrawer">
         <el-icon :size="20"><Setting /></el-icon>
-      </div>
-      <div class="username">{{ PROJECT_AUTHOR_NAME }}</div>
-      <div class="avatar">
-        <el-image :src="PROJECT_AUTHOR_AVATAR" fit="cover" loading="lazy" lazy alt="avatar" />
       </div>
     </div>
   </div>
@@ -91,33 +141,42 @@ const handleShare = () => {
 
     .pointer-lock {
       @apply flex justify-center items-center;
+      @apply cursor-pointer;
     }
 
     .fullscreen {
       @apply flex justify-center items-center;
+      @apply cursor-pointer;
     }
 
     .share {
       @apply flex justify-center items-center;
+      @apply cursor-pointer;
     }
 
-    .avatar {
-      @apply w-8 h-8;
-      @apply overflow-hidden;
+    .user {
+      @apply flex justify-center items-center;
 
-      :deep(img) {
-        @apply w-8 h-8;
-        @apply rounded-full;
+      .avatar {
+        @apply w-8 h-8 mx-1;
+        @apply overflow-hidden;
+
+        :deep(img) {
+          @apply w-8 h-8;
+          @apply rounded-full;
+          @apply select-none;
+        }
+      }
+
+      .username {
+        @apply text-base mx-1;
         @apply select-none;
       }
     }
 
-    .username {
-      @apply cursor-default select-none;
-    }
-
     .settings {
       @apply flex justify-center items-center;
+      @apply cursor-pointer;
     }
   }
 }

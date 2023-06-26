22a98e94c1c79ebad1a7822c61207b52d2f6b5fd
skyclouds2001 <skyclouds2001@163.com>
2023-06-26 12:00:45 +0800

    fix: small type update

--- packages/shooks/src/useModel.ts
+++ packages/shooks/src/useModel.ts
@@ -1,6 +1,6 @@
 const map = new WeakMap()
 
-const useModel = <T extends (...args: any[]) => any = (...args: any[]) => any>(hook: T): ReturnType<T> => {
+const useModel = <T extends (...args: any[]) => any>(hook: T): ReturnType<T> => {
   if (!map.has(hook)) {
     map.set(hook, hook())
   }

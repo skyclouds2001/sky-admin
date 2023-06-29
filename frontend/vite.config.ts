73e9dfe5b855269f4b077efa173be2bfa4509533
skyclouds2001 <skyclouds2001@163.com>
2023-06-29 17:08:03 +0800

    build: add ws cors support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -77,6 +77,11 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
       },
+      '/socket.io/': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        ws: true,
+      },
     },
   },
   preview: {

1e6cbc6b82fe516fabb5669c706e926a3e2c1d9e
skyclouds2001 <skyclouds2001@163.com>
2023-06-29 18:22:42 +0800

    build: small changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
       },
-      '/socket.io/': {
+      '/socket.io': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         ws: true,

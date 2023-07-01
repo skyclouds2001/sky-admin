d9105ceb5e20cc6a7ff7d38bfb9558ffe2d4b27f
skyclouds2001 <skyclouds2001@163.com>
2023-07-01 16:18:23 +0800

    feat: add basic performance view

--- frontend/src/views/monitor/SystemPerformance.vue
+++ frontend/src/views/monitor/SystemPerformance.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ElCard, ElSpace } from 'element-plus'
+import { useWebVitals } from '@/hook'
+
+const { LCPs, FIDs, CLSs } = useWebVitals()
+</script>
 
 <template>
-  <div>performance</div>
+  <el-space size="large" class="w-full p-4">
+    <el-card shadow="always" header="LCP">{{ LCPs }}</el-card>
+    <el-card shadow="always" header="FID">{{ FIDs }}</el-card>
+    <el-card shadow="always" header="CLS">{{ CLSs }}</el-card>
+  </el-space>
 </template>
 
 <style scoped lang="scss"></style>

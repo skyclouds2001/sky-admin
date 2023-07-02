c783fda8df9586d6ad848ddda29a08eb32ad9e4f
skyclouds2001 <skyclouds2001@163.com>
2023-07-02 17:08:47 +0800

    style: small changes

--- frontend/src/views/3D/Base3D.vue
+++ frontend/src/views/3D/Base3D.vue
@@ -11,10 +11,10 @@ const animation = (time: number) => {
   renderer.render(scene, camera)
 }
 
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10)
-camera.position.z = 1
-
 const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.2, 10)
+camera.position.z = 1
 
 const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
 const material = new THREE.MeshNormalMaterial()

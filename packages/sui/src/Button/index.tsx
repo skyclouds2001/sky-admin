f314d2ef1b184326c7676e8f2562b69fa636732b
skyclouds2001 <skyclouds2001@163.com>
2023-07-03 15:13:41 +0800

    feat: update button component

--- packages/sui/src/Button/index.tsx
+++ packages/sui/src/Button/index.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, type PropType } from 'vue'
+import { defineComponent, type PropType, type SlotsType } from 'vue'
 import './index.css'
 
 const Button = defineComponent({
@@ -9,17 +9,30 @@ const Button = defineComponent({
       required: false,
       validator: (value: string) => value === '' || ['primary', 'success', 'warning', 'danger', 'info'].includes(value),
     },
+    disabled: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
     nativeType: {
       type: String as PropType<'button' | 'submit' | 'reset'>,
       default: 'button',
       required: false,
       validator: (value: string) => ['button', 'submit', 'reset'].includes(value),
     },
+    autofocus: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
   },
+  slots: Object as SlotsType<{
+    default: () => any
+  }>,
   setup: (props, { slots }) => {
     return () => (
-      <button type={props.nativeType} class={['sky-button', props.type === 'primary' ? 'sky-button-primary' : '', props.type === 'success' ? 'sky-button-success' : '', props.type === 'warning' ? 'sky-button-warning' : '', props.type === 'danger' ? 'sky-button-danger' : '', props.type === 'info' ? 'sky-button-info' : '']}>
-        <span>{slots.default?.()}</span>
+      <button aria-disabled={props.disabled} disabled={props.disabled} type={props.nativeType} autofocus={props.autofocus} class={['sky-button', props.type === 'primary' ? 'sky-button-primary' : '', props.type === 'success' ? 'sky-button-success' : '', props.type === 'warning' ? 'sky-button-warning' : '', props.type === 'danger' ? 'sky-button-danger' : '', props.type === 'info' ? 'sky-button-info' : '', props.disabled ? 'sky-button-disabled' : '']}>
+        <span>{slots.default()}</span>
       </button>
     )
   },

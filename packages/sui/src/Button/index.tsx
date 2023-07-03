8a1ba2dc3eaa7d0abf4e532f7bf43d430a1214be
skyclouds2001 <skyclouds2001@163.com>
2023-07-03 12:38:17 +0800

    feat: update button component

--- packages/sui/src/Button/index.tsx
+++ packages/sui/src/Button/index.tsx
@@ -1,18 +1,24 @@
-import { defineComponent } from 'vue'
+import { defineComponent, type PropType } from 'vue'
 import './index.css'
 
 const Button = defineComponent({
   props: {
     type: {
-      type: String,
+      type: String as PropType<'primary' | 'success' | 'warning' | 'danger' | 'info'>,
       default: '',
       required: false,
-      validate: (value: string) => ['primary', 'success', 'warning', 'danger', 'info'].includes(value),
+      validator: (value: string) => value === '' || ['primary', 'success', 'warning', 'danger', 'info'].includes(value),
+    },
+    nativeType: {
+      type: String as PropType<'button' | 'submit' | 'reset'>,
+      default: 'button',
+      required: false,
+      validator: (value: string) => ['button', 'submit', 'reset'].includes(value),
     },
   },
   setup: (props, { slots }) => {
     return () => (
-      <button class={['sky-button', props.type === 'primary' ? 'sky-button-primary' : '', props.type === 'success' ? 'sky-button-success' : '', props.type === 'warning' ? 'sky-button-warning' : '', props.type === 'danger' ? 'sky-button-danger' : '', props.type === 'info' ? 'sky-button-info' : '']}>
+      <button type={props.nativeType} class={['sky-button', props.type === 'primary' ? 'sky-button-primary' : '', props.type === 'success' ? 'sky-button-success' : '', props.type === 'warning' ? 'sky-button-warning' : '', props.type === 'danger' ? 'sky-button-danger' : '', props.type === 'info' ? 'sky-button-info' : '']}>
         <span>{slots.default?.()}</span>
       </button>
     )

619b202d15eb7ff3dd4681e084f88e32da01a654
skyclouds2001 <skyclouds2001@163.com>
2023-07-04 15:41:56 +0800

    feat: update util of Obj type

--- packages/shooks/src/util.ts
+++ packages/shooks/src/util.ts
@@ -1,2 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Fn = (...args: any[]) => any
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Obj = Record<string, any>

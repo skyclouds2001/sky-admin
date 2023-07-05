553232e18ec1b740361a7e41dff4164c311dd09f
skyclouds2001 <skyclouds2001@163.com>
2023-07-06 00:38:47 +0800

    feat: update button component

--- packages/sui/src/Button/index.tsx
+++ packages/sui/src/Button/index.tsx
@@ -14,6 +14,11 @@ const Button = defineComponent({
       default: false,
       required: false,
     },
+    circle: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
     disabled: {
       type: Boolean,
       default: false,
@@ -42,7 +47,7 @@ const Button = defineComponent({
   }>,
   setup: (props, { slots }) => {
     return () => (
-      <button aria-disabled={props.disabled} disabled={props.disabled} type={props.nativeType} autofocus={props.autofocus} class={['sky-button', props.type !== 'default' ? `sky-button-${props.type}` : '', props.round ? 'sky-button-round' : '', props.disabled ? 'sky-button-disabled' : '', props.size !== 'default' ? `sky-button-${props.size}` : '']}>
+      <button aria-disabled={props.disabled} disabled={props.disabled} type={props.nativeType} autofocus={props.autofocus} class={['sky-button', props.type !== 'default' ? `sky-button-${props.type}` : '', props.round ? 'sky-button-round' : '', props.circle ? 'sky-button-circle' : '', props.disabled ? 'sky-button-disabled' : '', props.size !== 'default' ? `sky-button-${props.size}` : '']}>
         <span>{slots.default()}</span>
       </button>
     )

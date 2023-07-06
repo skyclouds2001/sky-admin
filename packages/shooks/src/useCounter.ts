722ae926ffa09c678ebbbb59858a9c4e619e799c
skyclouds2001 <skyclouds2001@163.com>
2023-07-06 20:53:55 +0800

    feat: update useCounter hook

--- packages/shooks/src/useCounter.ts
+++ packages/shooks/src/useCounter.ts
@@ -7,26 +7,34 @@ const useCounter = (
     initial?: number
   } = {}
 ): {
-  count: Readonly<Ref<number>>
-  increase: (delta: number) => void
-  decrease: (delta: number) => void
-  get: () => number
-  set: (val: number) => number
-  reset: () => void
-} => {
+    count: Readonly<Ref<number>>
+    increase: (delta?: number) => void
+    decrease: (delta?: number) => void
+    get: () => number
+    set: (val: number) => void
+    reset: () => void
+  } => {
   const { min = -Infinity, max = Infinity, initial = 0 } = options
 
   const count = ref(Math.max(min, Math.min(max, initial)))
 
-  const increase = (delta = 1) => (count.value = Math.min(max, count.value + delta))
+  const increase = (delta = 1): void => {
+    count.value = Math.min(max, count.value + delta)
+  }
 
-  const decrease = (delta = 1) => (count.value = Math.max(min, count.value - delta))
+  const decrease = (delta = 1): void => {
+    count.value = Math.max(min, count.value - delta)
+  }
 
-  const get = () => count.value
+  const get = (): number => count.value
 
-  const set = (val: number) => (count.value = Math.max(min, Math.min(max, val)))
+  const set = (val: number): void => {
+    count.value = Math.max(min, Math.min(max, val))
+  }
 
-  const reset = () => (count.value = Math.max(min, Math.min(max, initial)))
+  const reset = (): void => {
+    count.value = Math.max(min, Math.min(max, initial))
+  }
 
   return {
     count: readonly(count),

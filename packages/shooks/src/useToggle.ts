bfa93161516d20f57b583327a7687f9c41f3188d
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-07-06 10:20:10 +0800

    feat: update useToggle

--- packages/shooks/src/useToggle.ts
+++ packages/shooks/src/useToggle.ts
@@ -1,9 +1,15 @@
 import { type MaybeRefOrGetter, ref, type Ref, toValue } from 'vue'
 
-const useToggle = <T = true, F = false>(truthy: MaybeRefOrGetter<T>, falsy: MaybeRefOrGetter<F>, initial: T | F): {
+const useToggle = <T = true, F = false>(options: {
+    truthy?: MaybeRefOrGetter<T>
+    falsy?: MaybeRefOrGetter<F>
+    initial?: T | F
+} = {}): {
     value: Ref<T | F>
     toggle: () => void
 } => {
+    const { truthy = true, falsy = false, initial = falsy } = options
+
     const value = ref(initial) as Ref<T | F>
 
     const toggle = (val?: T | F): void => {

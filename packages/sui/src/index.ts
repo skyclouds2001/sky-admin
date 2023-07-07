5f72d23a360a731a17a4e38742667df4134cd324
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-07-07 17:50:06 +0800

    feat: add export

--- packages/sui/src/index.ts
+++ packages/sui/src/index.ts
@@ -2,4 +2,8 @@ import './index.css'
 
 export { default as Button } from './Button'
 
+export { default as Divider } from './Divider'
+
+export { default as Link } from './Link'
+
 export { default as Text } from './Text'

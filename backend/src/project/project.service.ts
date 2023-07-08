82d04eb47fae4c6d0e10f188247e2e5e5d322be7
skyclouds2001 <skyclouds2001@163.com>
2023-07-08 20:41:34 +0800

    feat: update project information

--- backend/src/project/project.service.ts
+++ backend/src/project/project.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@nestjs/common'
 import { HttpService } from '@nestjs/axios'
-import { Observable } from 'rxjs'
-import { AxiosResponse } from 'axios'
+import { firstValueFrom } from 'rxjs'
 import { Project } from './entities/project.entity'
 
 @Injectable()
 export class ProjectService {
   constructor(private readonly httpService: HttpService) {}
 
-  getProjectInformation(): Observable<AxiosResponse<Project>> {
-    return this.httpService.get('https://api.github.com/repos/skyclouds2001/SkyAdmin')
+  async getProjectInformation() {
+    return (await firstValueFrom(this.httpService.get<Project>('https://api.github.com/repos/skyclouds2001/SkyAdmin'))).data
   }
 }

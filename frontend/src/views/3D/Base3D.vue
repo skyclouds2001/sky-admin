e676ed16559d2756ad261d9fd83c5a3075c59380
skyclouds2001 <skyclouds2001@163.com>
2023-07-10 22:48:03 +0800

    style: small format

--- frontend/src/views/3D/Base3D.vue
+++ frontend/src/views/3D/Base3D.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
+import { ElCard, ElSpace } from 'element-plus'
 import * as THREE from 'three'
 
-const el = ref<HTMLDivElement | null>(null)
+const demo = ref<HTMLDivElement | null>(null)
 
 onMounted(() => {
   const animation = (time: number): void => {
@@ -14,25 +15,28 @@ onMounted(() => {
 
   const scene = new THREE.Scene()
 
-  const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.2, 10)
+  const camera = new THREE.PerspectiveCamera(70, 800 / 600, 0.2, 10)
   camera.position.z = 1
 
   const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
   const material = new THREE.MeshNormalMaterial()
-
   const mesh = new THREE.Mesh(geometry, material)
   scene.add(mesh)
 
   const renderer = new THREE.WebGLRenderer({ antialias: true })
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setSize(1000, 1000)
   renderer.setAnimationLoop(animation)
 
-  el.value?.appendChild(renderer.domElement)
+  demo.value?.appendChild(renderer.domElement)
 })
 </script>
 
 <template>
-  <div ref="el"></div>
+  <el-space direction="vertical" fill size="large" class="px-10 py-4 w-full">
+    <el-card shadow="always">
+      <div ref="demo" style="width: 800px; height: 600px"></div>
+    </el-card>
+  </el-space>
 </template>
 
 <style scoped lang="scss"></style>

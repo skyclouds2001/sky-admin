847bcca0d2ae7ef45ea51eb4bb76ad405b9662c3
skyclouds2001 <skyclouds2001@163.com>
2023-07-11 22:56:16 +0800

    feat: add mirror text view

---
+++ frontend/src/views/3D/MirrorText.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { ElCard, ElSpace } from 'element-plus'
+import * as THREE from 'three'
+// @ts-expect-error can not find type definition for this file
+import { FontLoader } from 'three/addons/loaders/FontLoader'
+// @ts-expect-error can not find type definition for this file
+import { TextGeometry } from 'three/addons/geometries/TextGeometry'
+
+import optimer_regular from '@/assets/optimer_regular.typeface.json?url'
+
+const text = ref<HTMLDivElement | null>(null)
+
+THREE.Cache.enabled = true
+
+const tf = (): void => {
+  if (text.value === null) return
+
+  const render = (): void => {
+    camera.lookAt(target)
+
+    renderer.clear()
+    renderer.render(scene, camera)
+  }
+
+  const camera = new THREE.PerspectiveCamera(30, 800 / 400, 1, 1500)
+  camera.position.set(0, 400, 700)
+
+  const target = new THREE.Vector3(0, 150, 0)
+
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color(0x000000)
+  scene.fog = new THREE.Fog(0x000000, 250, 1400)
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4)
+  directionalLight.position.set(0, 0, 1).normalize()
+  scene.add(directionalLight)
+
+  const pointLight = new THREE.PointLight(0xffffff, 4.5, 0, 0)
+  pointLight.color.setHSL(Math.random(), 1, 0.5)
+  pointLight.position.set(0, 100, 90)
+  scene.add(pointLight)
+
+  const plane = new THREE.Mesh(new THREE.PlaneGeometry(10000, 10000), new THREE.MeshBasicMaterial({ color: 0xffffff, opacity: 0.5, transparent: true }))
+  plane.position.y = 100
+  plane.position.x = -Math.PI / 2
+  scene.add(plane)
+
+  const group = new THREE.Group()
+  group.position.y = 100
+  scene.add(group)
+
+  const materials = [new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true }), new THREE.MeshPhongMaterial({ color: 0xffffff })]
+
+  const loader = new FontLoader()
+  loader.load(
+    optimer_regular,
+    (font: unknown) => {
+      const textGeometry = new TextGeometry('Hello World', {
+        font,
+        size: 70,
+        height: 20,
+        curveSegments: 4,
+        bevelThickness: 2,
+        bevelSize: 1.5,
+        bevelEnabled: true,
+      })
+      textGeometry.computeBoundingBox()
+
+      const center = -0.5 * (textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x)
+
+      const textMesh1 = new THREE.Mesh(textGeometry, materials)
+      textMesh1.position.set(center, 30, 0)
+      textMesh1.rotation.set(0, 2 * Math.PI, 0)
+      group.add(textMesh1)
+
+      const textMesh2 = new THREE.Mesh(textGeometry, materials)
+      textMesh2.position.set(center, -30, 0)
+      textMesh2.rotation.set(Math.PI, 2 * Math.PI, 0)
+      group.add(textMesh2)
+    },
+    (event: ProgressEvent) => {
+      console.log((event.loaded / event.total) * 100)
+    },
+    (error: unknown) => {
+      console.error(error)
+    }
+  )
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(800, 400)
+  renderer.setAnimationLoop(render)
+  renderer.useLegacyLights = false
+  text.value.appendChild(renderer.domElement)
+
+  text.value.style.touchAction = 'none'
+}
+
+onMounted(() => {
+  tf()
+})
+</script>
+
+<template>
+  <el-space direction="vertical" fill size="large" class="px-10 py-4 w-full">
+    <el-card shadow="always">
+      <div ref="text" style="width: 800px; height: 400px"></div>
+    </el-card>
+  </el-space>
+</template>
+
+<style scoped lang="scss"></style>

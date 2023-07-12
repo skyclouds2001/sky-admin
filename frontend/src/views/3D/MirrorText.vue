6c6a1daa5056e1b133808c08646cfbf3a009c4be
skyclouds2001 <skyclouds2001@163.com>
2023-07-12 21:48:19 +0800

    feat: support for input

--- frontend/src/views/3D/MirrorText.vue
+++ frontend/src/views/3D/MirrorText.vue
@@ -6,6 +6,7 @@ import * as THREE from 'three'
 import { FontLoader } from 'three/addons/loaders/FontLoader'
 // @ts-expect-error can not find type definition for this file
 import { TextGeometry } from 'three/addons/geometries/TextGeometry'
+import { useEventListener } from '@sky-fly/shooks'
 import { optimer_regular } from '@/assets'
 
 const text = ref<HTMLDivElement | null>(null)
@@ -16,16 +17,12 @@ const tf = (): void => {
   if (text.value === null) return
 
   const render = (): void => {
-    camera.lookAt(target)
-
-    renderer.clear()
     renderer.render(scene, camera)
   }
 
   const camera = new THREE.PerspectiveCamera(30, 800 / 400, 1, 1500)
   camera.position.set(0, 400, 700)
-
-  const target = new THREE.Vector3(0, 150, 0)
+  camera.lookAt(new THREE.Vector3(0, 150, 0))
 
   const scene = new THREE.Scene()
   scene.background = new THREE.Color(0x000000)
@@ -51,32 +48,17 @@ const tf = (): void => {
 
   const materials = [new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true }), new THREE.MeshPhongMaterial({ color: 0xffffff })]
 
-  const loader = new FontLoader()
-  loader.load(
+  let tt = 'Hello World'
+  let textMesh1: THREE.Mesh
+  let textMesh2: THREE.Mesh
+  let font: unknown
+
+  new FontLoader().load(
     optimer_regular,
-    (font: unknown) => {
-      const textGeometry = new TextGeometry('Hello World', {
-        font,
-        size: 70,
-        height: 20,
-        curveSegments: 4,
-        bevelThickness: 2,
-        bevelSize: 1.5,
-        bevelEnabled: true,
-      })
-      textGeometry.computeBoundingBox()
+    (result: unknown) => {
+      font = result
 
-      const center = -0.5 * (textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x)
-
-      const textMesh1 = new THREE.Mesh(textGeometry, materials)
-      textMesh1.position.set(center, 30, 0)
-      textMesh1.rotation.set(0, 2 * Math.PI, 0)
-      group.add(textMesh1)
-
-      const textMesh2 = new THREE.Mesh(textGeometry, materials)
-      textMesh2.position.set(center, -30, 0)
-      textMesh2.rotation.set(Math.PI, 2 * Math.PI, 0)
-      group.add(textMesh2)
+      renderText()
     },
     (event: ProgressEvent) => {
       console.log((event.loaded / event.total) * 100)
@@ -86,6 +68,31 @@ const tf = (): void => {
     }
   )
 
+  const renderText = (): void => {
+    const textGeometry = new TextGeometry(tt, {
+      font,
+      size: 70,
+      height: 20,
+      curveSegments: 4,
+      bevelThickness: 2,
+      bevelSize: 1.5,
+      bevelEnabled: true,
+    })
+    textGeometry.computeBoundingBox()
+
+    const center = -0.5 * (textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x)
+
+    textMesh1 = new THREE.Mesh(textGeometry, materials)
+    textMesh1.position.set(center, 30, 0)
+    textMesh1.rotation.set(0, 2 * Math.PI, 0)
+    group.add(textMesh1)
+
+    textMesh2 = new THREE.Mesh(textGeometry, materials)
+    textMesh2.position.set(center, -30, 0)
+    textMesh2.rotation.set(Math.PI, 2 * Math.PI, 0)
+    group.add(textMesh2)
+  }
+
   const renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(800, 400)
@@ -94,6 +101,19 @@ const tf = (): void => {
   text.value.appendChild(renderer.domElement)
 
   text.value.style.touchAction = 'none'
+  useEventListener(window, 'keydown', (e) => {
+    if (e.key === 'Backspace') {
+      tt = tt.slice(0, -1)
+    } else {
+      tt += e.key
+    }
+
+    group.clear()
+
+    if (tt.length === 0) return
+
+    renderText()
+  })
 }
 
 onMounted(() => {

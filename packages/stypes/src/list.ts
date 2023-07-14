375df1156435cff43a9b7bf60060edbe28f25bac
c50034865 <chensiyuan49@huawei.com>
2023-07-14 15:42:39 +0800

    feat: add builtin tool type

--- packages/stypes/src/list.ts
+++ packages/stypes/src/list.ts
@@ -1,3 +1,15 @@
+export type Partial<T> = {
+  [P in keyof T]?: T[P]
+}
+
+export type Required<T> = {
+  [P in keyof T]-?: T[P]
+}
+
+export type Readonly<T> = {
+  readonly [P in keyof T]: T[P]
+}
+
 export type Writable<T> = {
   -readonly [P in keyof T]: T[P]
 }

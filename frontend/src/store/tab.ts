7195776f60e081888fb3a40f1631ca6d4d68f3b3
skyclouds2001 <skyclouds2001@163.com>
2023-07-16 17:54:58 +0800

    feat: move away initial value

--- frontend/src/store/tab.ts
+++ frontend/src/store/tab.ts
@@ -5,15 +5,9 @@ const useTabsStore = defineStore('tabs', {
   state() {
     return {
       /** 标签页 */
-      tabs: [
-        {
-          name: '首页',
-          path: '/home',
-          isView: true,
-        },
-      ] as Array<Tab & { isView: boolean }>,
+      tabs: [] as Array<Tab & { isView: boolean }>,
       /** 当前标签页 */
-      currentTab: '/home',
+      currentTab: '/',
     }
   },
 })

28a8b4eda9af92fd8a955d130f3de24080e0a032
skyclouds2001 <skyclouds2001@163.com>
2023-07-19 21:33:08 +0800

    feat: add basic active building

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,3 +1,15 @@
 export { default as avatar } from './avatar.jpg'
 
+export { default as nx } from './nx.jpg'
+
+export { default as ny } from './ny.jpg'
+
+export { default as nz } from './nz.jpg'
+
 export { default as optimer_regular } from './optimer_regular.typeface.json?url'
+
+export { default as px } from './px.jpg'
+
+export { default as py } from './py.jpg'
+
+export { default as pz } from './pz.jpg'
--- frontend/src/views/3D/ActiveBuilding.vue
+++ frontend/src/views/3D/ActiveBuilding.vue
@@ -1,7 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { AmbientLight, Color, CubeTextureLoader, DirectionalLight, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three'
+import { nx, ny, nz, px, py, pz } from '@/assets'
+
+const container = ref<HTMLDivElement | null>(null)
+
+const cameraPosition = new Vector3(0, -13, 48)
+
+onMounted(() => {
+  if (container.value === null) return
+
+  const { width, height } = container.value.getBoundingClientRect()
+
+  const camera = new PerspectiveCamera(45, width / height, 1, 1000)
+  camera.position.copy(cameraPosition)
+
+  const scene = new Scene()
+  scene.background = new CubeTextureLoader().load([px, nx, py, ny, pz, nz].map((path) => new URL(path, import.meta.url).href))
+
+  const ambientLight = new AmbientLight(new Color(255, 255, 255))
+  scene.add(ambientLight)
+
+  const directionLight = new DirectionalLight(new Color(255, 99, 71), 2)
+  directionLight.position.set(-220, 30, 50)
+  scene.add(directionLight)
+
+  const renderer = new WebGLRenderer({
+    antialias: true,
+  })
+  renderer.setSize(width, height)
+  container.value.appendChild(renderer.domElement)
+
+  renderer.render(scene, camera)
+})
+</script>
 
 <template>
-  <div></div>
+  <div id="container" ref="container"></div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+#container {
+  width: 100%;
+  height: calc(100vh - 60px - 40px - 60px);
+}
+</style>

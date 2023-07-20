efc72aba01133b96b05a2a17d19bab4e79da0e8f
skyclouds2001 <skyclouds2001@163.com>
2023-07-21 00:53:53 +0800

    feat: update active  building

--- frontend/src/views/3D/ActiveBuilding.vue
+++ frontend/src/views/3D/ActiveBuilding.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { AmbientLight, Color, CubeTextureLoader, DirectionalLight, Euler, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three'
+import { AmbientLight, AxesHelper, Color, CubeTextureLoader, DirectionalLight, Euler, LoadingManager, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three'
+// @ts-expect-error can not find type definition for this file
+import { OrbitControls } from 'three/addons/controls/OrbitControls'
 // @ts-expect-error can not find type definition for this file
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader'
 // @ts-expect-error can not find type definition for this file
@@ -16,11 +18,13 @@ const cameraPosition = new Vector3(0, -13, 48)
 
 const modalPosition = new Vector3(14, -40.8, 0)
 
+const manager = new LoadingManager()
+
 const dracoLoader = new DRACOLoader()
 dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
 dracoLoader.preload()
 
-const gltfLoader = new GLTFLoader()
+const gltfLoader = new GLTFLoader(manager)
 gltfLoader.setDRACOLoader(gltfLoader)
 
 onMounted(() => {
@@ -28,6 +32,7 @@ onMounted(() => {
 
   const animate = (): void => {
     renderer.render(scene, camera)
+    control.update()
   }
 
   const { width, height } = container.value.getBoundingClientRect()
@@ -37,7 +42,7 @@ onMounted(() => {
   camera.lookAt(0, 0, 0)
 
   const scene = new Scene()
-  scene.background = new CubeTextureLoader().load(skyMaps)
+  scene.background = new CubeTextureLoader(manager).load(skyMaps)
 
   const ambientLight = new AmbientLight(new Color(255, 255, 255))
   scene.add(ambientLight)
@@ -49,13 +54,28 @@ onMounted(() => {
   const renderer = new WebGLRenderer({
     antialias: true,
   })
+  renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(width, height)
   renderer.setAnimationLoop(animate)
 
   container.value.appendChild(renderer.domElement)
 
+  const control = new OrbitControls(camera, renderer.domElement)
+  control.enabled = true
+  control.enableDamping = true
+  control.enableZoom = true
+  control.autoRotate = false
+  control.autoRotateSpeed = 0.1
+  control.enablePan = true
+  control.zoomSpeed = 1
+  control.minDistance = 0
+  control.maxDistance = Infinity
+
+  const axes = new AxesHelper(50)
+  axes.visible = false
+  scene.add(axes)
+
   gltfLoader.load(new URL(building, import.meta.url).href, (modal: any) => {
-    console.log({ ...modal })
     modal.scene.position.copy(modalPosition)
 
     const r: Euler = modal.scene.rotation.clone()
@@ -68,10 +88,10 @@ onMounted(() => {
     if (container.value === null) return
 
     const { width, height } = container.value.getBoundingClientRect()
-
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+    renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(width, height)
-
-    renderer.render(scene, camera)
   })
 })
 </script>

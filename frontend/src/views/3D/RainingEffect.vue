86a7c6c64dc7d0caf8889a6a7b272636ddb6950e
skyclouds2001 <skyclouds2001@163.com>
2023-07-23 18:52:53 +0800

    feat: small updates

--- frontend/src/views/3D/RainingEffect.vue
+++ frontend/src/views/3D/RainingEffect.vue
@@ -2,6 +2,8 @@
 import { onMounted, ref } from 'vue'
 import { AdditiveBlending, BufferAttribute, BufferGeometry, PerspectiveCamera, Points, PointsMaterial, Scene, TextureLoader, WebGLRenderer } from 'three'
 // @ts-expect-error can not find type definition for this file
+import Stats from 'three/addons/libs/stats.module'
+// @ts-expect-error can not find type definition for this file
 import { OrbitControls } from 'three/addons/controls/OrbitControls'
 import { useEventListener } from '@sky-fly/shooks'
 import { rain } from '@/assets'
@@ -11,6 +13,7 @@ const container = ref<HTMLDivElement | null>(null)
 onMounted(() => {
   const render = (): void => {
     controls.update()
+    stats.update()
     renderer.render(scene, camera)
 
     const p = points.geometry.getAttribute('position').array as Float32Array
@@ -29,14 +32,34 @@ onMounted(() => {
 
   if (container.value === null) return
 
-  const POINT_COUNT = 1000
-
   const { width, height } = container.value.getBoundingClientRect()
+
+  const stats = new Stats()
+  stats.domElement.style.position = 'absolute'
+  stats.domElement.style.top = '0'
+  container.value.appendChild(stats.domElement)
 
   const camera = new PerspectiveCamera(45, width / height, 1, 1000)
   camera.position.set(0, 0, 40)
+  camera.up.set(0, 1, 0)
+  camera.lookAt(0, 0, 0)
 
   const scene = new Scene()
+
+  const renderer = new WebGLRenderer()
+  renderer.setSize(width, height)
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setAnimationLoop(render)
+
+  container.value.appendChild(renderer.domElement)
+
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableDamping = true
+  controls.enableZoom = true
+  controls.autoRotate = false
+  controls.enablePan = true
+
+  const POINT_COUNT = 1000
 
   const geometry = new BufferGeometry()
   const positions = new Float32Array(POINT_COUNT * 3).fill(0).map(() => (Math.random() - 0.5) * 100)
@@ -62,19 +85,6 @@ onMounted(() => {
   const points = new Points(geometry, material)
   scene.add(points)
 
-  const renderer = new WebGLRenderer()
-  renderer.setSize(width, height)
-  renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setAnimationLoop(render)
-
-  container.value.appendChild(renderer.domElement)
-
-  const controls = new OrbitControls(camera, renderer.domElement)
-  controls.enableDamping = true
-  controls.enableZoom = true
-  controls.autoRotate = false
-  controls.enablePan = true
-
   useEventListener(window, 'resize', () => {
     if (container.value === null) return
 
@@ -94,6 +104,7 @@ onMounted(() => {
 
 <style scoped lang="scss">
 #container {
+  position: relative;
   width: 100%;
   height: calc(100vh - 60px - 40px - 60px);
 }

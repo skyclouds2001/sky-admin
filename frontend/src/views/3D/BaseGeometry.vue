06042ee06eb29f228f583489ef1e4ccb0e4a20fb
skyclouds2001 <skyclouds2001@163.com>
2023-08-02 15:27:18 +0800

    feat: add basic geometry view

--- frontend/src/views/3D/BaseGeometry.vue
+++ frontend/src/views/3D/BaseGeometry.vue
@@ -1,67 +1,240 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { ElCard, ElSpace } from 'element-plus'
-import { BoxGeometry, CapsuleGeometry, CircleGeometry, ConeGeometry, CylinderGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three'
+import { AmbientLight, BoxGeometry, CapsuleGeometry, CircleGeometry, Color, ConeGeometry, CylinderGeometry, DirectionalLight, DodecahedronGeometry, ExtrudeGeometry, IcosahedronGeometry, LatheGeometry, Mesh, MeshPhongMaterial, OctahedronGeometry, PerspectiveCamera, PlaneGeometry, RingGeometry, Scene, ShapeGeometry, SphereGeometry, TetrahedronGeometry, TorusGeometry, TorusKnotGeometry, TubeGeometry, WebGLRenderer } from 'three'
+import WebGL from 'three/examples/jsm/capabilities/WebGL'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { useEventListener } from '@sky-fly/shooks'
 
-const geometry = ref<HTMLCanvasElement | null>(null)
+const container = ref<HTMLDivElement | null>(null)
 
 onMounted(() => {
-  const animation = (time: number): void => {
-    box.rotation.x = time / 1000
-    box.rotation.y = time / 1000
-    capsule.rotation.x = time / 1000
-    capsule.rotation.y = time / 1000
-    circle.rotation.x = time / 1000
-    circle.rotation.y = time / 1000
-    cone.rotation.x = time / 1000
-    cone.rotation.y = time / 1000
-    cylinder.rotation.x = time / 1000
-    cylinder.rotation.y = time / 1000
-
+  const animate = (): void => {
+    controls.update()
+    stats.update()
     renderer.render(scene, camera)
   }
 
-  if (geometry.value === null) return
+  if (!WebGL.isWebGLAvailable()) return
 
-  const camera = new PerspectiveCamera(70, 800 / 400, 1, 100)
-  camera.position.set(0, 0, 10)
-  camera.lookAt(0, 0, 0)
+  if (container.value === null) return
+
+  const { width, height } = container.value.getBoundingClientRect()
+
+  const stats = new Stats()
+  stats.dom.style.position = 'absolute'
+  stats.dom.style.top = '0'
+  stats.dom.style.left = '0'
+  container.value.appendChild(stats.dom)
 
   const scene = new Scene()
+  scene.background = new Color(0x000000)
 
-  const box = new Mesh(new BoxGeometry(1, 1, 1, 2, 2, 2), new MeshBasicMaterial({ color: 0x00ff00 }))
-  box.position.set(10, 0, 0)
+  const camera = new PerspectiveCamera(50, width / height, 1, 1000)
+  camera.position.set(5, 5, 5)
+  camera.up.set(0, 1, 0)
+  camera.lookAt(0, 0, 0)
+
+  const renderer = new WebGLRenderer({
+    antialias: true,
+  })
+  renderer.setSize(width, height)
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setAnimationLoop(animate)
+  renderer.shadowMap.enabled = true
+
+  container.value.appendChild(renderer.domElement)
+
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableDamping = true
+
+  scene.add(new AmbientLight(0xffffff, 0.1))
+
+  const light = new DirectionalLight(0xffffff, 3)
+  light.position.set(300, 200, 100)
+  light.castShadow = true
+  scene.add(light)
+
+  const plane = new Mesh(new PlaneGeometry(50, 50), new MeshPhongMaterial({
+    color: 0xeeeeee,
+  }))
+  plane.position.set(0, -0.5, 0)
+  plane.rotation.set(-Math.PI / 2, 0, 0)
+  plane.castShadow = true
+  plane.receiveShadow = true
+  scene.add(plane)
+
+  const box = new Mesh(new BoxGeometry(), new MeshPhongMaterial({
+    color: 0x00ff00,
+  }))
+  box.position.set(0, 0, 2)
+  box.castShadow = true
+  box.receiveShadow = true
   scene.add(box)
 
-  const capsule = new Mesh(new CapsuleGeometry(1, 2, 8, 8), new MeshBasicMaterial({ color: 0xffff00 }))
-  capsule.position.set(-10, 0, 0)
+  const capsule = new Mesh(new CapsuleGeometry(), new MeshPhongMaterial({
+    color: 0x0000ff,
+  }))
+  capsule.position.set(2, 0, 2)
+  capsule.castShadow = true
+  capsule.receiveShadow = true
   scene.add(capsule)
 
-  const circle = new Mesh(new CircleGeometry(1, 32, 0, 1.5 * Math.PI), new MeshBasicMaterial({ color: 0xff0000 }))
-  circle.position.set(0, 5, 0)
-  scene.add(circle)
-
-  const cone = new Mesh(new ConeGeometry(1.5, 2, 12, 12, false, 0, 1.5 * Math.PI), new MeshBasicMaterial({ color: 0x0000ff }))
-  cone.position.set(0, -5, 0)
+  const cone = new Mesh(new ConeGeometry(), new MeshPhongMaterial({
+    color: 0xff0000,
+  }))
+  cone.position.set(2, 0, 0)
+  cone.castShadow = true
+  cone.receiveShadow = true
   scene.add(cone)
 
-  const cylinder = new Mesh(new CylinderGeometry())
-  cylinder.position.set(0, 0, 0)
+  const cylinder = new Mesh(new CylinderGeometry(), new MeshPhongMaterial({
+    color: 0xffff00,
+  }))
+  cylinder.position.set(2, 0, -2)
+  cylinder.castShadow = true
+  cylinder.receiveShadow = true
   scene.add(cylinder)
 
-  const renderer = new WebGLRenderer({ canvas: geometry.value, antialias: true })
-  renderer.setSize(800, 400)
-  renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setAnimationLoop(animation)
+  const dodecahedron = new Mesh(new DodecahedronGeometry(), new MeshPhongMaterial({
+    color: 0xff00ff,
+  }))
+  dodecahedron.position.set(0, 0, -2)
+  dodecahedron.castShadow = true
+  dodecahedron.receiveShadow = true
+  scene.add(dodecahedron)
+
+  const extrude = new Mesh(new ExtrudeGeometry(), new MeshPhongMaterial({
+    color: 0x00ffff,
+  }))
+  extrude.position.set(-2, 0, -2)
+  extrude.castShadow = true
+  extrude.receiveShadow = true
+  scene.add(extrude)
+
+  const icosahedron = new Mesh(new IcosahedronGeometry(), new MeshPhongMaterial({
+    color: 0x990000,
+  }))
+  icosahedron.position.set(-2, 0, 0)
+  icosahedron.castShadow = true
+  icosahedron.receiveShadow = true
+  scene.add(icosahedron)
+
+  const octahedron = new Mesh(new OctahedronGeometry(), new MeshPhongMaterial({
+    color: 0x009900,
+  }))
+  octahedron.position.set(-2, 0, 2)
+  octahedron.castShadow = true
+  octahedron.receiveShadow = true
+  scene.add(octahedron)
+
+  const sphere = new Mesh(new SphereGeometry(), new MeshPhongMaterial({
+    color: 0x000099,
+  }))
+  sphere.position.set(4, 0, 4)
+  sphere.castShadow = true
+  sphere.receiveShadow = true
+  scene.add(sphere)
+
+  const tetrahedron = new Mesh(new TetrahedronGeometry(), new MeshPhongMaterial({
+    color: 0x999900,
+  }))
+  tetrahedron.position.set(4, 0, -4)
+  tetrahedron.castShadow = true
+  tetrahedron.receiveShadow = true
+  scene.add(tetrahedron)
+
+  const torus = new Mesh(new TorusGeometry(), new MeshPhongMaterial({
+    color: 0x990099,
+  }))
+  torus.position.set(-4, 0, -4)
+  torus.castShadow = true
+  torus.receiveShadow = true
+  scene.add(torus)
+
+  const torusKnot = new Mesh(new TorusKnotGeometry(), new MeshPhongMaterial({
+    color: 0x009999,
+  }))
+  torusKnot.position.set(-4, 0, 4)
+  torusKnot.castShadow = true
+  torusKnot.receiveShadow = true
+  scene.add(torusKnot)
+
+  const circle = new Mesh(new CircleGeometry(), new MeshPhongMaterial({
+    color: 0x000000,
+  }))
+  circle.position.set(0, 0, 4)
+  circle.castShadow = true
+  circle.receiveShadow = true
+  scene.add(circle)
+
+  const lathe = new Mesh(new LatheGeometry(), new MeshPhongMaterial({
+    color: 0x000000,
+  }))
+  lathe.position.set(4, 0, 0)
+  lathe.castShadow = true
+  lathe.receiveShadow = true
+  scene.add(lathe)
+
+  const ring = new Mesh(new RingGeometry(), new MeshPhongMaterial({
+    color: 0x000000,
+  }))
+  ring.position.set(-4, 0, 0)
+  ring.castShadow = true
+  ring.receiveShadow = true
+  scene.add(ring)
+
+  const shape = new Mesh(new ShapeGeometry(), new MeshPhongMaterial({
+    color: 0x000000,
+  }))
+  shape.position.set(0, 0, -4)
+  shape.castShadow = true
+  shape.receiveShadow = true
+  scene.add(shape)
+
+  const tube = new Mesh(new TubeGeometry(), new MeshPhongMaterial({
+    color: 0x000000,
+  }))
+  tube.position.set(0, 2, 0)
+  tube.castShadow = true
+  tube.receiveShadow = true
+  scene.add(tube)
+
+  useEventListener(window, 'resize', () => {
+    if (container.value === null) return
+
+    const { width, height } = container.value.getBoundingClientRect()
+
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+    renderer.setSize(width, height)
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.render(scene, camera)
+  })
+
+  useEventListener(window.screen.orientation, 'orientationchange', () => {
+    if (container.value === null) return
+
+    const { width, height } = container.value.getBoundingClientRect()
+
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+    renderer.setSize(width, height)
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.render(scene, camera)
+  })
 })
 </script>
 
 <template>
-  <el-space direction="vertical" fill size="large" class="px-10 py-4 w-full">
-    <el-card shadow="always">
-      <canvas ref="geometry" width="800" height="400" style="width: 800px; height: 400px"></canvas>
-    </el-card>
-  </el-space>
+  <div v-if="WebGL.isWebGLAvailable()" id="container" ref="container"></div>
+  <div v-else>WebGL is not supported by current version of browser, please update to the newest version of browser.</div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped>
+#container {
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 60px - 40px - 60px);
+}
+</style>

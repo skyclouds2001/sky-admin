1cd32fec1b92a15af171ae6e931b89196c1218f8
skyclouds2001 <95597335+skyclouds2001@users.noreply.github.com>
2023-09-06 14:07:55 +0800

    Update index.ts

--- packages/vite-plugin-copy-assets/index.ts
+++ packages/vite-plugin-copy-assets/index.ts
@@ -2,9 +2,17 @@ import { createLogger, normalizePath, type Plugin, type ResolvedConfig } from 'v
 import path from 'node:path'
 import fs from 'node:fs'
 
+const logger = createLogger()
+
 type MaybeArray<T> = T | T[]
 
+/**
+ * asset properties
+ */
 interface Asset {
+  /**
+   * asset source position, should be an relative path to root dictionary or an absolute path
+   */
   source: string
 }
 
@@ -37,8 +45,6 @@ const CopyAssets = (assets: MaybeArray<Asset>, options: CopyAssetOptions = {}): 
       config = cf
     },
     closeBundle: () => {
-      const logger = createLogger()
-
       let {
         root,
         build: { outDir },
@@ -54,8 +60,10 @@ const CopyAssets = (assets: MaybeArray<Asset>, options: CopyAssetOptions = {}): 
       const mode = overwrite ? fs.constants.COPYFILE_FICLONE : fs.constants.COPYFILE_EXCL
 
       assets.forEach((asset) => {
-        const origin = path.resolve(root, normalizePath(asset.source))
-        const target = path.resolve(outDir)
+        const src = normalizePath(asset.source)
+
+        const origin = path.resolve(root, src)
+        const target = path.resolve(outDir, path.basename(src))
 
         fs.copyFile(origin, target, mode, (err) => {
           if (err != null) {

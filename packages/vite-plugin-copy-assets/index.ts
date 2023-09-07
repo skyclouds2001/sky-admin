e7be14b4c073938f2488e80ff8f81a58c85c311c
skyclouds2001 <skyclouds2001@163.com>
2023-09-07 13:28:20 +0800

    feat: support serve in dev mode

--- packages/vite-plugin-copy-assets/index.ts
+++ packages/vite-plugin-copy-assets/index.ts
@@ -83,6 +83,51 @@ const CopyAssets = (assets: MaybeArray<Asset>, options: CopyAssetOptions = {}): 
         })
       })
     },
+    configureServer: (server) => {
+      const mappers = new Map<string, string>()
+
+      const root = normalizePath(options.root ?? config.root)
+      const outDir = normalizePath(options.out ?? config.build.outDir)
+
+      if (!Array.isArray(assets)) {
+        assets = [assets]
+      }
+
+      assets.forEach((asset) => {
+        const src = normalizePath(asset.source)
+
+        const origin = path.resolve(root, src)
+        const target = path.resolve(outDir, asset.target != null && !path.isAbsolute(asset.target) ? normalizePath(asset.target) : path.basename(src))
+
+        mappers.set(origin, target)
+      })
+
+      server.middlewares.use((req, res, next) => {
+        if (req.url == null) {
+          next()
+          return
+        }
+
+        const link = mappers.get(req.url)
+
+        if (link == null) {
+          next()
+          return
+        }
+
+        // eslint-disable-next-line security/detect-non-literal-fs-filename
+        fs.readFile(link, (err, data) => {
+          if (err != null) {
+            next()
+            return
+          }
+
+          res.writeHead(200)
+          res.write(data)
+          res.end()
+        })
+      })
+    },
   }
 }
 

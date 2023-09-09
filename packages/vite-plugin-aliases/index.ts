bb80bc586393f798d3237fbb1b906b425dcf636b
skyclouds2001 <skyclouds2001@163.com>
2023-09-09 23:29:20 +0800

    feat: update generate alias plugin

--- packages/vite-plugin-aliases/index.ts
+++ packages/vite-plugin-aliases/index.ts
@@ -1,59 +1,47 @@
-import type { Plugin, AliasOptions } from 'vite'
-import fs from 'node:fs'
+import type { Plugin } from 'vite'
 import path from 'node:path'
+import fs from 'node:fs'
 
-const addAlias = (aliases: AliasOptions, alias: string, path: string): void => {
-  if (!aliases.hasOwnProperty(alias)) {
-    Object.defineProperty(aliases, alias, {
-      value: path,
-    })
-  }
-}
-
-const generateAlias = (alias: AliasOptions, { enable = true, root = process.cwd(), dir = 'src', prefix = '@', log = true, logPath = path.resolve(process.cwd(), 'logs'), logFile = 'vite-plugin-auto-aliases.log' }: PluginConfig): AliasOptions => {
-  try {
-    const src = path.resolve(root, dir)
-    addAlias(alias, prefix, src)
-
-    if (enable) {
-      const fds = fs.readdirSync(src)
-      fds.forEach((v) => {
-        const fss = path.resolve(src, v)
-        const fstat = fs.statSync(fss)
-        if (fstat.isDirectory()) {
-          addAlias(alias, prefix + v, fss)
-        }
-      })
-    }
-  } catch (err) {
-    console.error('[ERROR] VITE_PLUGIN_AUTO_ALIASES: ', err)
-    try {
-      if (log) {
-        const error = err as Error
-        fs.writeFileSync(path.resolve(logPath, logFile), error.name + ' : ' + error.message)
-      }
-    } catch {}
-  }
-  return alias
-}
-
-interface PluginConfig {
-  enable?: boolean
+interface AliasesOptions {
   root?: string
   dir?: string
   prefix?: string
-  log?: boolean
-  logPath?: string
-  logFile?: string
 }
 
-export default function (pluginConfig: PluginConfig = {}): Plugin {
+/**
+ * 
+ * @param options plugin options
+ * @returns plugin instance
+ */
+const Aliases = (options: AliasesOptions = {}): Plugin => {
+  const { root = process.cwd(), dir = 'src', prefix = '@' } = options
+
   return {
-    name: 'vite-plugin-auto-aliases',
-    config: (config) => ({
-      resolve: {
-        alias: generateAlias(config.resolve?.alias ?? {}, pluginConfig),
-      },
-    }),
+    name: 'vite-plugin-aliases',
+    config: () => {
+      const aliases = new Map<string, string>()
+
+      const base = path.resolve(root, dir)
+
+      try {
+        aliases.set(prefix, base)
+
+        const routes = fs.readdirSync(base)
+
+        routes.forEach((route) => {
+          aliases.set(prefix + route, path.resolve(base, route))
+        })
+      } catch (error) {
+        console.error(error)
+      }
+
+      return {
+        resolve: {
+          alias: Object.fromEntries(aliases),
+        },
+      }
+    },
   }
 }
+
+export default Aliases

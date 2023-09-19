a0d4308295e91e48f6143eaf5c0d1a030f6d8e96
skyclouds2001 <skyclouds2001@163.com>
2023-09-19 17:48:38 +0800

    fix: update alias check

--- packages/vite-plugin-aliases/index.ts
+++ packages/vite-plugin-aliases/index.ts
@@ -1,4 +1,4 @@
-import { createLogger, type Plugin } from 'vite'
+import { createFilter, createLogger, type Plugin } from 'vite'
 import path from 'node:path'
 import fs from 'node:fs'
 
@@ -7,8 +7,14 @@ import fs from 'node:fs'
  */
 interface AliasesOptions {
   /**
-   * the base path to generate alias info
-   * @default `process.cwd()+'src'`
+   * the root path of the project
+   * @default process.cwd()
+   */
+  root?: string
+
+  /**
+   * the base path to generate alias info, should be an relative path to `options.root`
+   * @default 'src'
    */
   base?: string
 
@@ -33,29 +39,35 @@ interface AliasesOptions {
 const Aliases = (options: AliasesOptions = {}): Plugin => {
   const logger = createLogger()
 
-  const { base = process.cwd() + '@', prefix = '@', overwrite = false } = options
+  const { root = process.cwd(), base = 'src', prefix = '@', overwrite = false } = options
 
   return {
     name: 'vite-plugin-aliases',
     config: (config) => {
       const alias = config.resolve?.alias ?? {}
 
-      const hasAlias = (key: string): boolean => (Array.isArray(alias) ? alias.some((v) => v.find === alias || v.find?.test(alias)) : Object.hasOwn(alias, key))
+      const hasAlias = (key: string): boolean => (Array.isArray(alias) ? alias.some((v) => v.find === key || v.find?.test(key)) : Object.hasOwn(alias, key))
 
       const aliases = new Map<string, string>()
 
+      const basis = path.resolve(root, base)
+
       try {
-        if (!hasAlias(prefix) || overwrite) {
-          aliases.set(prefix, base)
+        if (fs.statSync(basis).isDirectory() && (!hasAlias(prefix) || overwrite)) {
+          aliases.set(prefix, basis)
         }
 
-        const routes = fs.readdirSync(base)
+        const routes = fs.readdirSync(basis)
 
         routes.forEach((route) => {
-          if (!hasAlias(prefix) || overwrite) {
-            aliases.set(prefix + route, path.resolve(base, route))
+          const src = path.resolve(basis, route)
+
+          if (fs.statSync(src).isDirectory() && (!hasAlias(prefix + route) || overwrite)) {
+            aliases.set(prefix + route, src)
           }
         })
+
+        logger.info(aliases)
       } catch (error) {
         logger.info(String(error))
       }

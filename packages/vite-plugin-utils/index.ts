36d7b11ac4bc625912a9204688513b60d999be04
skyclouds2001 <skyclouds2001@163.com>
2023-09-30 23:29:53 +0800

    feat: update

--- packages/vite-plugin-utils/index.ts
+++ packages/vite-plugin-utils/index.ts
@@ -1,57 +1,86 @@
-import type { Plugin, UserConfig } from 'vite'
+import type { Plugin } from 'vite'
 
 /**
  * plugin options
  */
 interface UtilsOptions {
-  /**
-   * whether generate environment env of build time
-   * @default true
-   */
-  buildTime?: boolean
+  // vite hooks
 
   /**
-   * whether enable  cross-origin isolation state mode
-   * @default true
+   * @see https://vitejs.dev/guide/api-plugin.html#config
    */
-  crossOriginIsolated?: boolean
+  config: Plugin['config']
+
+  /**
+   * @see https://vitejs.dev/guide/api-plugin.html#configresolved
+   */
+  configResolved: Plugin['configResolved']
+
+  /**
+   * @see https://vitejs.dev/guide/api-plugin.html#configureserver
+   */
+  configureServer: Plugin['configureServer']
+
+  /**
+   * @see https://vitejs.dev/guide/api-plugin.html#configurepreviewserver
+   */
+  configurePreviewServer: Plugin['configurePreviewServer']
+
+  /**
+   * @see https://vitejs.dev/guide/api-plugin.html#transformindexhtml
+   */
+  transformIndexHtml: Plugin['transformIndexHtml']
+
+  /**
+   * @see https://vitejs.dev/guide/api-plugin.html#handlehotupdate
+   */
+  handleHotUpdate: Plugin['handleHotUpdate']
+
+  /**
+   * @see https://rollupjs.org/plugin-development/#options
+   */
+  options: Plugin['options']
+
+  /**
+   * @see https://rollupjs.org/plugin-development/#buildstart
+   */
+  buildStart: Plugin['buildStart']
+
+  /**
+   * @see https://rollupjs.org/plugin-development/#resolveid
+   */
+  resolveId: Plugin['resolveId']
+
+  /**
+   * @see https://rollupjs.org/plugin-development/#load
+   */
+  load: Plugin['load']
+
+  /**
+   * @see https://rollupjs.org/plugin-development/#transform
+   */
+  transform: Plugin['transform']
+
+  /**
+   * @see https://rollupjs.org/plugin-development/#buildend
+   */
+  buildEnd: Plugin['buildEnd']
+
+  /**
+   * @see https://rollupjs.org/plugin-development/#closebundle
+   */
+  closeBundle: Plugin['closeBundle']
 }
 
 /**
- * a plugin for maintain fantastic utils
+ * a plugin for expose hooks for develop
  * @param options plugin options
  * @returns plugin instance
  */
-const Utils = (options: UtilsOptions = {}): Plugin => {
-  const { buildTime = true, crossOriginIsolated = true } = options
-
-  const config: UserConfig = {}
-
-  if (buildTime) {
-    config.define = {
-      __BUILD_TIME__: `'${new Date().toLocaleString()}'`,
-    }
-  }
-
-  if (crossOriginIsolated) {
-    config.server = {
-      headers: {
-        'Cross-Origin-Opener-Policy': 'same-origin',
-        'Cross-Origin-Embedder-Policy': 'require-corp',
-      },
-    }
-    config.preview = {
-      headers: {
-        'Cross-Origin-Opener-Policy': 'same-origin',
-        'Cross-Origin-Embedder-Policy': 'require-corp',
-      },
-    }
-  }
-
+const Utils = (options: Partial<UtilsOptions> = {}): Plugin => {
   return {
     name: 'vite-plugin-utils',
-
-    config: () => ({ ...config }),
+    ...options,
   }
 }
 

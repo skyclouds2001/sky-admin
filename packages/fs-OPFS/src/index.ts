77ef464497598d898719d4d8eaf917c4c04e19d7
skyclouds2001 <skyclouds2001@163.com>
2023-10-13 11:04:04 +0800

    feat: update

--- packages/fs-OPFS/src/index.ts
+++ packages/fs-OPFS/src/index.ts
@@ -8,7 +8,7 @@ const root = await window.navigator.storage.getDirectory()
 
 const resolvePath = (path: string): string[] => {
   if (!path.startsWith('/') || path.endsWith('/')) {
-    throw new Error('Invalid path')
+    throw new TypeError('Invalid path')
   }
 
   const paths = path.split('/')
@@ -17,7 +17,7 @@ const resolvePath = (path: string): string[] => {
 }
 
 const getFileHandle = async (path: string, options: { create?: boolean } = {}): Promise<FileSystemFileHandle> => {
-  const { create = true } = options
+  const { create = false } = options
   const paths = resolvePath(path)
   const len = paths.length
 
@@ -34,10 +34,36 @@ const getFileHandle = async (path: string, options: { create?: boolean } = {}): 
   return fileHandle
 }
 
-export const readFile = async (path: string): Promise<File> => {
-  const handle = await getFileHandle(path)
+export const readFile = async (path: string, options: { create?: boolean } = {}): Promise<File> => {
+  const { create = false } = options
+
+  const handle = await getFileHandle(path, { create })
 
   const file = await handle.getFile()
 
   return file
+}
+
+export const readFileAsText = async (path: string, options: { create?: boolean } = {}): Promise<string> => {
+  const file = await readFile(path, options)
+
+  const text = await file.text()
+
+  return text
+}
+
+export const readFileAsBuffer = async (path: string, options: { create?: boolean } = {}): Promise<ArrayBuffer> => {
+  const file = await readFile(path, options)
+
+  const buffer = await file.arrayBuffer()
+
+  return buffer
+}
+
+export const readFileAsStream = async (path: string, options: { create?: boolean } = {}): Promise<ReadableStream<Uint8Array>> => {
+  const file = await readFile(path, options)
+
+  const stream = file.stream()
+
+  return stream
 }

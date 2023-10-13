95a4e687240ff52324769ca241798d8db184d857
skyclouds2001 <skyclouds2001@163.com>
2023-10-14 00:58:08 +0800

    build: support for fs-opfs package

---
+++ packages/fs-OPFS/rollup.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'rollup'
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+
+export default defineConfig({
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+    },
+  ],
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+    }),
+    resolve({
+      extensions: ['.ts', '.tsx', '.mts', 'cts', '.js', '.jsx', '.mjs', 'cjs', '.json'],
+    }),
+    typescript({
+      sourceMap: false,
+      declaration: true,
+      declarationDir: 'dist',
+      rootDir: './src',
+      exclude: ['node_modules/**/*', 'dist/**/*', 'test/**/*'],
+    }),
+    terser(),
+  ],
+})

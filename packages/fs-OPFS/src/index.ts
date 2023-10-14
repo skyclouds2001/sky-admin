6228a2d700d91426f33228598c38e70a93d445e7
skyclouds2001 <skyclouds2001@163.com>
2023-10-14 20:45:02 +0800

    feat: update

--- packages/fs-OPFS/src/index.ts
+++ packages/fs-OPFS/src/index.ts
@@ -1,7 +1,16 @@
+/**
+ * package name
+ */
 export const NAME = 'fs-OPFS'
 
+/**
+ * package version
+ */
 export const VERSION = '0.0.0'
 
+/**
+ * package support status
+ */
 export const isSupported = 'storage' in window.navigator && 'getDirectory' in window.navigator.storage
 
 const root = await window.navigator.storage.getDirectory()
@@ -20,6 +29,12 @@ interface GetHandleOptions {
   create?: boolean
 }
 
+/**
+ * get file handle in the given path
+ * @param path given path
+ * @param options read additional options
+ * @returns the file handle
+ */
 export const getFileHandle = async (path: string, options: GetHandleOptions = {}): Promise<FileSystemFileHandle> => {
   const { create = false } = options
   const paths = resolvePath(path)
@@ -38,6 +53,12 @@ export const getFileHandle = async (path: string, options: GetHandleOptions = {}
   return fileHandle
 }
 
+/**
+ * get dictionary handle in the given path
+ * @param path given path
+ * @param options read additional options
+ * @returns the dictionary handle
+ */
 export const getDirHandle = async (path: string, options: GetHandleOptions = {}): Promise<FileSystemDirectoryHandle> => {
   const { create = false } = options
   const paths = resolvePath(path)
@@ -57,16 +78,30 @@ interface ReadFileOptions {
   create?: boolean
 }
 
+/**
+ * read file contents in the given path
+ * @param path given path
+ * @param options read additional options
+ * @returns the content of file as a `File` object
+ */
 export const readFile = async (path: string, options: ReadFileOptions = {}): Promise<File> => {
   const { create = false } = options
 
-  const handle = await getFileHandle(path, { create })
+  const handle = await getFileHandle(path, {
+    create,
+  })
 
   const file = await handle.getFile()
 
   return file
 }
 
+/**
+ * read file contents in the given path
+ * @param path given path
+ * @param options read additional options
+ * @returns the content of file as string
+ */
 export const readFileAsText = async (path: string, options: ReadFileOptions = {}): Promise<string> => {
   const file = await readFile(path, options)
 
@@ -75,6 +110,12 @@ export const readFileAsText = async (path: string, options: ReadFileOptions = {}
   return text
 }
 
+/**
+ * read file contents in the given path
+ * @param path given path
+ * @param options read additional options
+ * @returns the content of file as a `ArrayBuffer` object
+ */
 export const readFileAsBuffer = async (path: string, options: ReadFileOptions = {}): Promise<ArrayBuffer> => {
   const file = await readFile(path, options)
 
@@ -83,6 +124,12 @@ export const readFileAsBuffer = async (path: string, options: ReadFileOptions = 
   return buffer
 }
 
+/**
+ * read file contents in the given path
+ * @param path given path
+ * @param options read additional options
+ * @returns the content of file as a `ReadableStream` object
+ */
 export const readFileAsStream = async (path: string, options: ReadFileOptions = {}): Promise<ReadableStream<Uint8Array>> => {
   const file = await readFile(path, options)
 
@@ -95,10 +142,18 @@ interface ReadDirOptions {
   create?: boolean
 }
 
+/**
+ * read dictionary contents in the given path
+ * @param path given path
+ * @param options read additional options
+ * @returns the name of files or dictionaries as an array of string
+ */
 export const readDir = async (path: string, options: ReadDirOptions = {}): Promise<string[]> => {
   const { create = false } = options
 
-  const handle = await getDirHandle(path, { create })
+  const handle = await getDirHandle(path, {
+    create,
+  })
 
   const keys: string[] = []
 
@@ -108,4 +163,38 @@ export const readDir = async (path: string, options: ReadDirOptions = {}): Promi
   }
 
   return keys
+}
+
+interface WriteFileOptions {
+  create?: boolean
+  position?: number
+  useExisted?: boolean
+}
+
+type WriteFileData = string | Blob | ArrayBuffer | ArrayBufferView | DataView
+
+/**
+ * write data to the specific file  in the given path
+ * @param path given path
+ * @param data data to be written to the specific file
+ * @param options write additional options
+ */
+export const writeFile = async (path: string, data: WriteFileData, options: WriteFileOptions = {}): Promise<void> => {
+  const { create = false, position, useExisted = false } = options
+
+  const handle = await getFileHandle(path, {
+    create,
+  })
+
+  const stream = await handle.createWritable({
+    keepExistingData: useExisted,
+  })
+
+  await stream.write({
+    type: 'write',
+    data,
+    position,
+  })
+
+  await stream.close()
 }

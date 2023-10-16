62e970200869ff1fcd8382c2ad07e7b55fee511f
skyclouds2001 <skyclouds2001@163.com>
2023-10-16 22:43:08 +0800

    feat: copy

--- packages/fs-OPFS/src/index.ts
+++ packages/fs-OPFS/src/index.ts
@@ -10,6 +10,8 @@ export const VERSION = '0.0.0'
 
 export { isSupported } from './init'
 
+export { copyDir, copyFile } from './copy'
+
 export { makeDir } from './make'
 
 export { openDir, openFile } from './open'

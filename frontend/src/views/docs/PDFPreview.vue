24116f12ead7676530a68cb1e76ca825a5f531b6
skyclouds2001 <skyclouds2001@163.com>
2023-10-18 22:05:52 +0800

    feat: update pdf viewer

--- frontend/src/views/docs/PDFPreview.vue
+++ frontend/src/views/docs/PDFPreview.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import * as pdfjs from 'pdfjs-dist'
+import worker from 'pdfjs-dist/build/pdf.worker.min?url'
 import { DEMO_PDF_SRC } from '@/config'
+
+const canvas = ref<HTMLCanvasElement | null>(null)
+
+onMounted(async () => {
+  pdfjs.GlobalWorkerOptions.workerSrc = worker
+
+  const doc = await pdfjs.getDocument(new URL(DEMO_PDF_SRC, import.meta.url)).promise
+
+  const page = await doc.getPage(1)
+
+  const cvs = canvas.value as HTMLCanvasElement
+  const ctx = cvs.getContext('2d') as CanvasRenderingContext2D
+
+  const scale = window.devicePixelRatio ?? 1
+
+  const viewport = page.getViewport()
+
+  cvs.width = Math.floor(scale * viewport.width)
+  cvs.height = Math.floor(scale * viewport.height)
+  cvs.style.width = Math.floor(viewport.width) + 'px'
+  cvs.style.height = Math.floor(viewport.height) + 'px'
+
+  page.render({
+    canvasContext: ctx,
+    viewport,
+    transform: [scale, 0, 0, scale, 0, 0],
+  })
+})
 </script>
 
 <template>
-  <iframe class="pdf-preview" name="pdf-preview" title="pdf-preview" referrerpolicy="no-referrer" :src="DEMO_PDF_SRC"></iframe>
+  <canvas ref="canvas" class="pdf-preview"></canvas>
 </template>
 
 <style scoped lang="scss">

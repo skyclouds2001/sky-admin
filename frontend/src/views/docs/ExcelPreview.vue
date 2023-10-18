9afc5d579c49baccd9de67be4c440b824d783966
skyclouds2001 <skyclouds2001@163.com>
2023-10-18 16:20:46 +0800

    feat: update excel preview

--- frontend/src/views/docs/ExcelPreview.vue
+++ frontend/src/views/docs/ExcelPreview.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { read, utils } from 'xlsx'
 import { DEMO_EXCEL_SRC } from '@/config'
-import { generateDocumentLink } from '@/util'
+
+const contest = ref('')
+
+onMounted(() => {
+  fetch(DEMO_EXCEL_SRC)
+    .then((res) => res.arrayBuffer())
+    .then((buffer) => {
+      const workbook = read(buffer)
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+      const html = utils.sheet_to_html(worksheet)
+      contest.value = html
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+})
 </script>
 
 <template>
-  <iframe class="excel-preview" name="excel-preview" title="excel-preview" referrerpolicy="no-referrer" :src="generateDocumentLink(DEMO_EXCEL_SRC)"></iframe>
+  <!-- eslint-disable-next-line vue/no-v-html -->
+  <div class="excel-preview" v-html="contest"></div>
 </template>
 
 <style scoped lang="scss">
-.pdf-preview {
+.excel-preview {
   width: 100%;
   min-width: 1000px;
   height: 100%;

d20d697ad3529e4ba0727780c03665a549875ebe
skyclouds2001 <skyclouds2001@163.com>
2023-11-19 14:33:11 +0800

    feat: add 3d earth

--- frontend/src/views/map/3dEarth.vue
+++ frontend/src/views/map/3dEarth.vue
@@ -1,7 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Ion, Viewer } from 'cesium'
+import { ref, onMounted, onUnmounted } from 'vue'
+import 'cesium/Build/CesiumUnminified/Widgets/widgets.css'
+
+const el = ref<HTMLDivElement | null>(null)
+
+let viewer: Viewer
+
+window.CESIUM_BASE_URL = 'node_modules/cesium/Build/CesiumUnminified/'
+
+Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5YmRhMzFkYS1hMTI5LTQ2ZjEtODk3ZS0yNDFlYmNkYzk3OGMiLCJpZCI6MTc4OTM4LCJpYXQiOjE3MDAxOTYyNTB9.XmsCEAlfbOR7RqNm2zpp4qCuVkYdaBjsSXbrjcjSJew'
+
+onMounted(() => {
+  viewer = new Viewer(el.value as HTMLDivElement, {
+    fullscreenElement: el.value as HTMLDivElement,
+  })
+
+  // remove credit information
+  const credit = viewer.cesiumWidget.creditContainer as HTMLElement
+  credit.style.display = 'none'
+
+  // add sunshine support
+})
+
+onUnmounted(() => {
+  viewer.destroy()
+})
+</script>
 
 <template>
-  <div>3d-earth</div>
+  <div ref="el" class="earth"></div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.earth {
+  width: 100%;
+  height: 100%;
+}
+
+:global(:has(> .earth)) {
+  width: 100%;
+  height: 100%;
+}
+</style>

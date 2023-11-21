a17c57a51b572deaab25ab802c34a0704e1c50a9
skyclouds2001 <skyclouds2001@163.com>
2023-11-21 16:55:15 +0800

    feat: update model

--- frontend/src/model/index.ts
+++ frontend/src/model/index.ts
@@ -1,5 +1,7 @@
 export type { default as Article } from './article'
 
+export type { default as Department } from './department'
+
 export type { default as Message } from './message'
 
 export type { default as Notification } from './notification'
@@ -7,6 +9,8 @@ export type { default as Notification } from './notification'
 export type { default as Pagination } from './pagination'
 
 export type { default as Response } from './response'
+
+export type { default as Role } from './role'
 
 export type { default as Todo } from './todo'
 
--- frontend/src/model/user.ts
+++ frontend/src/model/user.ts
@@ -1,3 +1,5 @@
+import type { Department, Role } from '@/model'
+
 /**
  * 用户数据结构
  */
@@ -8,6 +10,14 @@ interface User {
   name: string
   /** 用户邮箱 */
   email: string
+  /** 用户所属部门 ID */
+  departmentId: number
+  /** 用户所属部门信息 */
+  department: Department
+  /** 用户所属角色 ID */
+  roleId: number
+  /** 用户所属角色信息 */
+  role: Role
   /** 数据创建时间 */
   createdAt: string
   /** 数据更新时间 */
